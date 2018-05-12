3dead749f4a3f2e5558a7f53d0c5fe83a0153a5e
sekharpanja <sekharpanja@gmail.com>
2018-05-12 23:01:01 +0100

    cleanup urls

--- config/urls.py
+++ config/urls.py
@@ -6,8 +6,6 @@ from api.ping.views import ping
 from api.user.views import who_am_i
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     path('ping.xml', ping, name='ping'),
     path('whoami/', who_am_i, name='who_am_i'),
-    url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
 ]
