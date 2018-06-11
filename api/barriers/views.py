b683dea54bc9f2bb80bc3e3fd344ed02a15d356a
sekharpanja <sekharpanja@gmail.com>
2018-06-11 15:05:09 +0100

    adding pagination to new barriers

--- api/barriers/views.py
+++ api/barriers/views.py
@@ -25,7 +25,14 @@ class BarrierList(generics.ListCreateAPIView):
             self.queryset = self.filter_queryset(self.get_queryset().filter(
                 status=self.kwargs['status'])
             )
-        serializer = self.get_serializer(self.get_queryset(), many=True)
+
+        queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
     def get(self, request, *args, **kwargs):
