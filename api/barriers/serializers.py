1cff09e2fc71e1b9972a3b0d0602af43f0fca049
sekharpanja <sekharpanja@gmail.com>
2018-06-11 18:49:14 +0100

    barrier update, tidy up

--- api/barriers/serializers.py
+++ api/barriers/serializers.py
@@ -4,6 +4,34 @@ from api.barriers.models import Barrier, BarrierReportStage, ReportStage
 from api.barriers.company import Company
 
 
+class ReportStageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReportStage
+        fields = '__all__'
+
+
+class BarrierReportStageSerializer(serializers.ModelSerializer):
+    stage = ReportStageSerializer(many=False)
+
+    class Meta:
+        model = BarrierReportStage
+        fields = '__all__'
+
+
+class BarrierDetailSerializer(serializers.ModelSerializer):
+    # report_stages = BarrierReportStageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Barrier
+        fields = '__all__'
+        read_only_fields = (
+            'id',
+            'created_on',
+            'created_by',
+        )
+        depth = 1
+
+
 class BarrierListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Barrier
@@ -26,21 +54,6 @@ class BarrierListSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         barrier = Barrier.objects.create(**validated_data)
         return barrier
-
-
-class BarrierDetailSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Barrier
-        fields = '__all__'
-        read_only_fields = (
-            'id',
-            'created_on',
-            'created_by',
-        )
-        depth = 2
-
-    def update(self, validated_data):
-        print(validated_data)
 
 
 class BarrierReportStageListSerializer(serializers.ModelSerializer):
