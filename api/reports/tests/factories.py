64ac8d031caeb682960f4aa92010fd3f48c1a2c3
sekharpanja <sekharpanja@gmail.com>
2018-06-25 11:30:17 +0100

    tests

--- api/reports/tests/factories.py
+++ api/reports/tests/factories.py
@@ -13,11 +13,41 @@ from api.metadata.constants import (
 )
 from api.reports.models import Report, ReportStage, Stage
 
+sample_companies = [
+    {
+        'id': '',
+        'name': '',
+        'contact': ''
+    },
+    {
+        'id': '',
+        'name': '',
+        'contact': ''
+    },
+    {
+        'id': '',
+        'name': '',
+        'contact': ''
+    }
+]
+
+
+class Stage1_1ReportFactory(factory.django.DjangoModelFactory):
+    problem_status = factory.fuzzy.FuzzyChoice(PROBLEM_STATUS_TYPES)
+    is_emergency = False
+    comapny = factory.fuzzy.FuzzyChoice(sample_companies)
+    company_id = company['id']
+    company_name = company['name']
+    contact_id = company['contact']
+
+    class Meta:
+        model = Report
+
 
 class ReportFactory(factory.django.DjangoModelFactory):
     problem_status = factory.fuzzy.FuzzyChoice(PROBLEM_STATUS_TYPES)
     is_emergency = False
-    company_id
+    company_id =
     company_name
     contact_id
     product
