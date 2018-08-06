a0c5c831ebe419dc3f6e95e651c399c3ef23f8c5
sekharpanja <sekharpanja@gmail.com>
2018-08-06 17:38:59 +0100

    change barrier_type description to text as it might have to hold long text

--- api/metadata/models.py
+++ api/metadata/models.py
@@ -8,7 +8,7 @@ from api.metadata.constants import BARRIER_TYPE_CATEGORIES
 class BarrierType(models.Model):
     # id = models.UUIDField(primary_key=True, default=uuid4)
     title = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
+    description = models.TextField()
     category = models.CharField(max_length=20, choices=BARRIER_TYPE_CATEGORIES)
 
     def __str__(self):
