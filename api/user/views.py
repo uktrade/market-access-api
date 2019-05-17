31dc6abdd18fc7be59a2141a3f6ca3f682dd0af3
sekharpanja <sekharpanja@gmail.com>
2019-05-17 13:27:53 +0100

    adding patch method to be able to edit existing profile

--- api/user/views.py
+++ api/user/views.py
@@ -1,16 +1,15 @@
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework import generics
 from rest_framework.response import Response
 
 from api.user.serializers import WhoAmISerializer
 
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'PATCH'])
 @permission_classes([])
 def who_am_i(request):
     """Return the current user. This view is behind a login."""
     serializer = WhoAmISerializer(request.user)
 
-    if request.method == 'POST':
+    if request.method == 'PATCH' or request.method == 'POST':
         return Response(data=request.data)
     return Response(data=serializer.data)
