fc7b8cb124113b43a41a3962bb8772eb6adf6922
sekharpanja <sekharpanja@gmail.com>
2019-05-22 23:03:00 +0100

    fixing patch

--- api/user/views.py
+++ api/user/views.py
@@ -10,6 +10,14 @@ def who_am_i(request):
     """Return the current user. This view is behind a login."""
     serializer = WhoAmISerializer(request.user)
 
-    if request.method == 'PATCH' or request.method == 'POST':
+    if request.method == 'PATCH':
+        req_profile = request.data.get("user_profile", None)
+        if req_profile:
+            if request.user.profile:
+                request.user.profile.user_profile = req_profile
+                request.user.profile.save()
+                request.user.save()
+
+    if request.method == 'POST':
         return Response(data=request.data)
     return Response(data=serializer.data)
