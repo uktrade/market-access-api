c5afb2fa0b3e6db8918a23a413167917756bbbc3
Sekhar Panja <sekharpanja@gmail.com>
2019-06-06 10:29:02 +0100

    another fix

--- api/user/views.py
+++ api/user/views.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
+from api.user.models import Profile, save_user_profile
 from api.user.serializers import WhoAmISerializer
 
 TOKEN_SESSION_KEY = '_authbroker_token'
@@ -18,10 +19,14 @@ def who_am_i(request):
     if request.method == 'PATCH':
         req_profile = request.data.get("user_profile", None)
         if req_profile:
-            if request.user.profile:
-                request.user.profile.user_profile = req_profile
-                request.user.profile.save()
-                request.user.save()
+            try:
+                profile = request.user.profile
+            except Profile.DoesNotExist:
+                Profile.objects.create(user=request.user)
+
+            request.user.profile.user_profile = req_profile
+            request.user.profile.save()
+            request.user.save()
 
     if request.method == 'POST':
         return Response(data=request.data)
