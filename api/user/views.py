aa1740e5b6f17d487999cb6a2e6fe1fd213e9ee9
Feri Csepregi <feri@31.digital>
2020-01-07 13:06:05 +0000

    MAR-145 - default email to 'email' field in sso details in case 'contact_email' is not present

--- api/user/views.py
+++ api/user/views.py
@@ -49,18 +49,22 @@ class UserDetail(generics.RetrieveDestroyAPIView):
             user = user_profile.user
         except Profile.DoesNotExist:
             sso_user = sso.get_user_details_by_id(sso_user_id)
+            email = sso_user.get("email")
+            contact_email = sso_user.get("contact_email") or email
+            first_name = sso_user.get("first_name")
+            last_name = sso_user.get("last_name")
             try:
-                user = UserModel.objects.get(username=sso_user["email"])
-                user.email = sso_user["contact_email"]
-                user.first_name = sso_user["first_name"]
-                user.last_name = sso_user["last_name"]
+                user = UserModel.objects.get(username=email)
+                user.email = contact_email
+                user.first_name = first_name
+                user.last_name = last_name
                 user.save()
             except UserModel.DoesNotExist:
                 user = UserModel(
-                    username=sso_user["email"],
-                    email=sso_user["contact_email"],
-                    first_name=sso_user["first_name"],
-                    last_name=sso_user["last_name"],
+                    username=email,
+                    email=contact_email,
+                    first_name=first_name,
+                    last_name=last_name,
                 )
                 user.save()
             user.profile.sso_user_id = sso_user_id
