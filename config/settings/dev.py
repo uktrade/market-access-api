34472e0b3263ce151c52df5fb7f1a43e34e33e48
Dan Kingston <d@nkingston.com>
2020-06-05 16:54:07 +0100

    Use ECSFormatter in all settings files

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,4 +1,30 @@
+import sys
 from .base import *
+
+from django_log_formatter_ecs import ECSFormatter
 
 DJANGO_ENV = 'dev'
 SSO_ENABLED = True
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "ecs_formatter": {
+            "()": ECSFormatter,
+        },
+    },
+    "handlers": {
+        "ecs": {
+            "formatter": "ecs_formatter",
+            "class": "logging.StreamHandler",
+            "stream": sys.stdout,
+        },
+    },
+    "loggers": {
+        "": {
+            "handlers": ["ecs"],
+            "level": DJANGO_LOG_LEVEL
+        }
+    },
+}
