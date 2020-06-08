1f5af6d2f96629bc7f0adc233fe1e4f8a7fb6ecc
dankingston <d@nkingston.com>
2020-06-08 12:09:24 +0100

    MAR-247: Add correlation ids to logging (#154)
    * MAR-247: Add correlation ids to logging

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,4 +1,31 @@
+import sys
 from .base import *
 
+from django_log_formatter_ecs import ECSFormatter
+
+# SECURITY WARNING: don't run with debug turned on in production!
 DJANGO_ENV = 'dev'
 SSO_ENABLED = True
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "ecs_formatter": {
+            "()": ECSFormatter,
+        },
+    },
+    "handlers": {
+        "ecs": {
+            "formatter": "ecs_formatter",
+            "class": "logging.StreamHandler",
+            "stream": sys.stdout,
+        },
+    },
+    "loggers": {
+        "": {
+            "handlers": ["ecs"],
+            "level": DJANGO_LOG_LEVEL
+        }
+    },
+}
--- config/settings/production.py
+++ config/settings/production.py
@@ -1,6 +1,32 @@
+import sys
 from .base import *
+
+from django_log_formatter_ecs import ECSFormatter
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 DJANGO_ENV = 'prod'
 SSO_ENABLED = True
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "ecs_formatter": {
+            "()": ECSFormatter,
+        },
+    },
+    "handlers": {
+        "ecs": {
+            "formatter": "ecs_formatter",
+            "class": "logging.StreamHandler",
+            "stream": sys.stdout,
+        },
+    },
+    "loggers": {
+        "": {
+            "handlers": ["ecs"],
+            "level": DJANGO_LOG_LEVEL
+        }
+    },
+}
