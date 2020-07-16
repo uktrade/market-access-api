0c5c7771c3db2d7352d8baad615b0bd2f33033a1
Feri Csepregi <feri@31.digital>
2020-07-16 13:01:30 +0100

    MAR-213 - user permissions in progress

---
+++ api/user/constants.py
@@ -0,0 +1,8 @@
+class UserRoles:
+    """
+    Group based roles, the groups were created in ./api/user/migrations/0015_assign_publish_permissions.py
+    """
+    SIFTER = "Sifter"
+    EDITOR = "Editor"
+    PUBLISHER = "Publisher"
+    ADMIN = "Administrator"
