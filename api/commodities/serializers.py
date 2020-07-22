829fa7e309049305e166c9150d7e98566f52917c
Dan Kingston <d@nkingston.com>
2020-07-22 17:00:39 +0100

    Use a through model for barrier commodities

--- api/commodities/serializers.py
+++ api/commodities/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from api.barriers.models import BarrierCommodity
 from .models import Commodity
 
 
@@ -14,4 +15,18 @@ class CommoditySerializer(serializers.ModelSerializer):
             "level",
             "description",
             "full_name",
+
+
+class BarrierCommoditySerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField(source='commodity.id')
+    description = serializers.ReadOnlyField(source='commodity.description')
+    full_description = serializers.ReadOnlyField(source='commodity.full_description')
+
+    class Meta:
+        model = BarrierCommodity
+        fields = (
+            "id",
+            "code",
+            "description",
+            "full_description",
         )
--- api/commodities/views.py
+++ api/commodities/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Max
+from django.http import Http404
 from rest_framework import generics
 
 from .models import Commodity
@@ -13,8 +15,13 @@ class CommodityList(generics.ListAPIView):
 
         codes = self.request.query_params.get("codes")
         if codes:
-            queryset = queryset.filter(code__in=codes.split(","))
-
+            hs6_codes = [code[:6].ljust(10, "0") for code in codes.split(",")]
+            latest_version = Commodity.objects.aggregate(Max('version')).get("version__max")
+            queryset = queryset.filter(
+                code__in=hs6_codes,
+                is_leaf=True,
+                version=latest_version,
+            ).order_by("code")
         return queryset
 
 
@@ -22,7 +29,12 @@ class CommodityDetail(generics.RetrieveAPIView):
     serializer_class = CommoditySerializer
 
     def get_object(self):
+        zero_padded_code = self.kwargs.get("code").ljust(10, "0")
+
         try:
-            return Commodity.objects.filter(code=self.kwargs.get("code")).latest("id")
+            return Commodity.objects.filter(
+                code=zero_padded_code,
+                is_leaf=True,
+            ).latest("version")
         except Commodity.DoesNotExist:
             raise Http404("Commodity does not exist")
