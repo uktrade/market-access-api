97b148b7ed7ec90abcb54ae42c17079f5101e56d
Dan Kingston <d@nkingston.com>
2020-07-22 17:06:46 +0100

    Add versions and SIDs to Commodity model

--- api/commodities/models.py
+++ api/commodities/models.py
@@ -2,14 +2,19 @@ from django.db import models
 
 
 class Commodity(models.Model):
+    version = models.CharField(max_length=32)
     code = models.CharField(max_length=10)
     suffix = models.CharField(max_length=2)
     description = models.TextField()
     level = models.IntegerField()
     indent = models.IntegerField()
     parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True)
+    is_leaf = models.BooleanField(default=False)
+    sid = models.IntegerField()
+    parent_sid = models.IntegerField(null=True)
 
-    def full_name(self):
-        if not self.parent:
-            return self.name
-        return f"{self.parent.name} - {self.name}"
+    @property
+    def full_description(self):
+        if not self.parent or self.level <= 4:
+            return self.description
+        return f"{self.parent.full_description} - {self.description}"
