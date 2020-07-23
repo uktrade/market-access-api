cf0ab6af4b54e95b2cc7c879c0142d2e5d8047bd
Dan Kingston <d@nkingston.com>
2020-07-23 16:53:59 +0100

    Add db indexing to Commodity lookup fields

--- api/commodities/models.py
+++ api/commodities/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class Commodity(models.Model):
-    version = models.CharField(max_length=32)
-    code = models.CharField(max_length=10)
+    version = models.CharField(max_length=32, db_index=True)
+    code = models.CharField(max_length=10, db_index=True)
     suffix = models.CharField(max_length=2)
     description = models.TextField()
     level = models.IntegerField()
     indent = models.IntegerField()
     parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True)
-    is_leaf = models.BooleanField(default=False)
+    is_leaf = models.BooleanField(default=False, db_index=True)
     sid = models.IntegerField()
     parent_sid = models.IntegerField(null=True)
 
