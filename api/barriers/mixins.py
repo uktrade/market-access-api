f0f33a1cda29d610223cc60c8b8782cc060a5ca0
Dan Kingston <d@nkingston.com>
2020-08-13 16:40:48 +0100

    Fix tests

--- api/barriers/mixins.py
+++ api/barriers/mixins.py
@@ -6,6 +6,8 @@ class BarrierRelatedMixin:
     """
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
+        self.barrier.skip_history_when_saving = True
         self.barrier.modified_on = self.modified_on
         self.barrier.modified_by = self.modified_by
         self.barrier.save()
+        del self.barrier.skip_history_when_saving
