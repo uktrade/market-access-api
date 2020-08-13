f76e7937f6c78d9da9eb74fa3fc0bac8564368b5
Dan Kingston <d@nkingston.com>
2020-08-13 11:48:28 +0100

    Deprecate old data workspace endpoint

--- api/dataset/views.py
+++ api/dataset/views.py
@@ -6,25 +6,6 @@ from rest_framework.permissions import IsAuthenticated
 from api.barriers.models import BarrierInstance
 from api.barriers.serializers import DataWorkspaceSerializer
 from api.dataset.pagination import MarketAccessDatasetViewCursorPagination
-from api.dataset.serializers import BarrierDataSetSerializer
-
-
-class BarrierListDataWorkspaceView(generics.ListAPIView):
-    """
-    Return a list of all the BarrierInstances
-    for Dataset to be consumed by Data-flow periodically
-    """
-
-    if settings.HAWK_ENABLED:
-        authentication_classes = (HawkAuthentication,)
-        permission_classes = (IsAuthenticated,)
-    else:
-        authentication_classes = ()
-        permission_classes = ()
-    pagination_class = MarketAccessDatasetViewCursorPagination
-
-    queryset = BarrierInstance.barriers.all().select_related('priority').order_by('reported_on')
-    serializer_class = BarrierDataSetSerializer
 
 
 class BarrierList(generics.ListAPIView):
--- tests/dataset/test_views.py
+++ tests/dataset/test_views.py
@@ -15,58 +15,18 @@ class TestBarriersDataset(APITestMixin):
 
     def test_no_reports(self):
         """Test there are no reports using list"""
-        url = reverse("dataset:barriers")
+        url = reverse("dataset:barrier-list")
         response = self.api_client.get(url)
         assert response.status_code == status.HTTP_200_OK
         assert response.data["results"] == []
 
-    def test_list_barriers_dataset_attributes(self):
-        spain = "86756b9a-5d95-e211-a939-e4115bead28a"
-        adv_engineering = "af959812-6095-e211-a939-e4115bead28a"
-        user = create_test_user(sso_user_id=self.sso_creator["user_id"])
-        db_barrier = BarrierFactory(
-            created_by=user,
-            export_country=spain,
-            sectors=(adv_engineering,),
-            status=2, status_date=datetime(2020, 1, 1, tzinfo=UTC),
-        )
-        TeamMember.objects.create(barrier=db_barrier, user=user, role="Wobble")
-
-        url = reverse("dataset:barriers")
-        response = self.api_client.get(url)
-
-        assert response.status_code == status.HTTP_200_OK
-        assert len(response.data["results"]) == 1
-        barrier = response.data["results"][0]
-        assert barrier["id"] == str(db_barrier.id)
-        assert barrier["code"] is not None
-        assert barrier["barrier_title"] is not None
-        assert barrier["status"] == "Open: In progress"
-        assert barrier["priority"] == "Unknown"
-        assert barrier["country"] == ["Spain"]
-        assert barrier["overseas_region"] == "Europe"
-        assert barrier["admin_areas"] == []
-        assert barrier["sectors"] == ["Advanced Engineering"]
-        assert barrier["product"] is not None
-        assert barrier["scope"] is not None
-        assert barrier["categories"] == []
-        assert barrier["source"] == "Company"
-        assert barrier["team_count"] == 1
-        assert barrier["status_date"] == "2020-01-01"
-        assert barrier["assessment_impact"] is None
-        assert barrier["value_to_economy"] is None
-        assert barrier["import_market_size"] is None
-        assert barrier["commercial_value"] is None
-        assert barrier["commercial_value_explanation"] is None
-        assert barrier["export_value"] is None
-
-    def test_list_barriers_dataset(self):
+    def test_list_barriers_count(self):
         count = 2
         BarrierFactory.create_batch(count)
 
         assert count == BarrierInstance.objects.count()
 
-        dataset_url = reverse("dataset:barriers")
+        dataset_url = reverse("dataset:barrier-list")
         response = self.api_client.get(dataset_url)
         assert status.HTTP_200_OK == response.status_code
         assert count == len(response.data["results"])
