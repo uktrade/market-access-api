c63693ce196847373368a00ec29e9fec03ed54e4
Dan Kingston <d@nkingston.com>
2020-10-12 17:00:09 +0100

    Ensure tag ordering test will pass as more tags get added

--- tests/metadata/test_models.py
+++ tests/metadata/test_models.py
@@ -7,12 +7,10 @@ from tests.metadata.factories import BarrierTagFactory
 class TestBarrierTags(TestCase):
 
     def test_tag_ordering(self):
-        # we have 2 default tags already in migrations
-        # so let's add a third
-        tag3 = BarrierTagFactory()
+        new_tag = BarrierTagFactory()
 
-        assert 3 == BarrierTag.objects.count()
-        assert 3 == tag3.order
+        tag_count = BarrierTag.objects.count()
+        assert tag_count == new_tag.order
 
         tags = BarrierTag.objects.all()
         assert 1 == tags[0].order
