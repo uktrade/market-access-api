f7024e976d36d6cd202a9157dba4ffedf179afd8
Dan Kingston <d@nkingston.com>
2020-11-24 16:40:13 +0000

    Fix import for missing sensitive code

--- api/assessment/automate/__init__.py
+++
@@ -1,8 +0,0 @@
-class AssessmentCalculator:
-    """
-    Stub class to avoid broken import.
-
-    Delete this when sensitivity issues around automated assessments are resolved
-    - use AssessmentCalculator instead
-    """
-    pass
---
+++ api/assessment/automate/calculator.py
@@ -0,0 +1,8 @@
+class AssessmentCalculator:
+    """
+    Stub class to avoid broken import.
+
+    Delete this when sensitivity issues around automated assessments are resolved
+    - use AssessmentCalculator instead
+    """
+    pass
