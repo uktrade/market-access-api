91ac968938b906c898b9f484d25bf8b6a7190057
Ciaran Doherty <cad106uk@gmail.com>
2021-02-08 13:52:55 +0000

    Non-functiobal change
    run black over files this fixes all quote markes ' -> "

--- config/settings/local.py
+++ config/settings/local.py
@@ -2,37 +2,38 @@ from django.utils.log import DEFAULT_LOGGING
 
 from . import base
 
+DJANGO_ENV = "local"
+
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'console': {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "console": {
             # exact format is not important, this is the minimum information
-            'format': '[%(asctime)s] %(name)s %(levelname)5s - %(message)s',
+            "format": "[%(asctime)s] %(name)s %(levelname)5s - %(message)s",
         },
-        'django.server': DEFAULT_LOGGING['formatters']['django.server'],
+        "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
     },
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'console',
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "console",
         },
-        'django.server': DEFAULT_LOGGING['handlers']['django.server'],
+        "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
     },
-    'loggers': {
+    "loggers": {
         # root logger
-        '': {
-            'level': 'WARNING',
-            'handlers': ['console'],
+        "": {
+            "level": "WARNING",
+            "handlers": ["console"],
         },
-        'market-access-python-frontend': {
-            'level': DJANGO_LOG_LEVEL,  # noqa
-            'handlers': ['console'],
+        "market-access-python-frontend": {
             "level": base.DJANGO_LOG_LEVEL,
+            "handlers": ["console"],
             # required to avoid double logging with root logger
-            'propagate': False,
+            "propagate": False,
         },
-        'django.server': DEFAULT_LOGGING['loggers']['django.server'],
+        "django.server": DEFAULT_LOGGING["loggers"]["django.server"],
     },
 }
 
--- config/urls.py
+++ config/urls.py
@@ -15,18 +15,26 @@ from api.user.views import UserDetail, who_am_i
 urlpatterns = []
 
 # Allow regular login to admin panel for local development
-if settings.DJANGO_ENV != 'local':
+if settings.DJANGO_ENV != "local":
     urlpatterns += [
         path("admin/login/", admin_override, name="override"),
     ]
 
-urlpatterns += [
-    path("admin/", admin.site.urls),
-    path("auth/", include("authbroker_client.urls", namespace="authbroker")),
-    path("", include("api.healthcheck.urls", namespace="healthcheck")),
-    path("whoami", who_am_i, name="who_am_i"),
-    path("users/<int:pk>", UserDetail.as_view(), name="get-user"),
-    path("users/<uuid:sso_user_id>", UserDetail.as_view(), name="get-user"),
-    path("metadata", MetadataView.as_view(), name="metadata"),
-    path("", include("api.dataset.urls", namespace="dataset")),
-] + barrier_urls + commodities_urls + interaction_urls + team_urls + assessment_urls + user_urls
+urlpatterns += (
+    [
+        path("admin/", admin.site.urls),
+        path("auth/", include("authbroker_client.urls", namespace="authbroker")),
+        path("", include("api.healthcheck.urls", namespace="healthcheck")),
+        path("whoami", who_am_i, name="who_am_i"),
+        path("users/<int:pk>", UserDetail.as_view(), name="get-user"),
+        path("users/<uuid:sso_user_id>", UserDetail.as_view(), name="get-user"),
+        path("metadata", MetadataView.as_view(), name="metadata"),
+        path("", include("api.dataset.urls", namespace="dataset")),
+    ]
+    + barrier_urls
+    + commodities_urls
+    + interaction_urls
+    + team_urls
+    + assessment_urls
+    + user_urls
+)
