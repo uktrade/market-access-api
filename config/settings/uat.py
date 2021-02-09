4c861fe60db43c78920cb322ef77a9e65fe298ef
Ciaran Doherty <cad106uk@gmail.com>
2021-02-09 16:44:53 +0000

    Non-functional: Change running black over code

--- config/settings/uat.py
+++ config/settings/uat.py
@@ -1,11 +1,12 @@
 import sys
-from .base import *
 
 from django_log_formatter_ecs import ECSFormatter
 
+from .base import *
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-DJANGO_ENV = 'uat'
+DJANGO_ENV = "uat"
 SSO_ENABLED = True
 
 LOGGING = {
@@ -23,10 +24,5 @@ LOGGING = {
             "stream": sys.stdout,
         },
     },
-    "loggers": {
-        "": {
-            "handlers": ["ecs"],
-            "level": DJANGO_LOG_LEVEL
-        }
-    },
+    "loggers": {"": {"handlers": ["ecs"], "level": DJANGO_LOG_LEVEL}},
 }
--- config/urls.py
+++ config/urls.py
@@ -1,32 +1,40 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
-from api.metadata.views import MetadataView
-from api.user.views import who_am_i, UserDetail
-from api.core.views import admin_override
 from api.assessment.urls import urlpatterns as assessment_urls
 from api.barriers.urls import urlpatterns as barrier_urls
 from api.collaboration.urls import urlpatterns as team_urls
 from api.commodities.urls import urlpatterns as commodities_urls
+from api.core.views import admin_override
 from api.interactions.urls import urlpatterns as interaction_urls
+from api.metadata.views import MetadataView
 from api.user.urls import urlpatterns as user_urls
+from api.user.views import UserDetail, who_am_i
 
 urlpatterns = []
 
 # Allow regular login to admin panel for local development
-if settings.DJANGO_ENV != 'local':
+if settings.DJANGO_ENV != "local":
     urlpatterns += [
         path("admin/login/", admin_override, name="override"),
     ]
 
-urlpatterns += [
-    path("admin/", admin.site.urls),
-    path("auth/", include("authbroker_client.urls", namespace="authbroker")),
-    path("", include("api.healthcheck.urls", namespace="healthcheck")),
-    path("whoami", who_am_i, name="who_am_i"),
-    path("users/<int:pk>", UserDetail.as_view(), name="get-user"),
-    path("users/<uuid:sso_user_id>", UserDetail.as_view(), name="get-user"),
-    path("metadata", MetadataView.as_view(), name="metadata"),
-    path("", include("api.dataset.urls", namespace="dataset")),
-] + barrier_urls + commodities_urls + interaction_urls + team_urls + assessment_urls + user_urls
+urlpatterns += (
+    [
+        path("admin/", admin.site.urls),
+        path("auth/", include("authbroker_client.urls", namespace="authbroker")),
+        path("", include("api.healthcheck.urls", namespace="healthcheck")),
+        path("whoami", who_am_i, name="who_am_i"),
+        path("users/<int:pk>", UserDetail.as_view(), name="get-user"),
+        path("users/<uuid:sso_user_id>", UserDetail.as_view(), name="get-user"),
+        path("metadata", MetadataView.as_view(), name="metadata"),
+        path("", include("api.dataset.urls", namespace="dataset")),
+    ]
+    + barrier_urls
+    + commodities_urls
+    + interaction_urls
+    + team_urls
+    + assessment_urls
+    + user_urls
+)
