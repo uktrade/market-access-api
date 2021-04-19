228135fb469589d3787bbd1345b2eac26c11f767
Ciaran Doherty <cad106uk@gmail.com>
2021-04-19 16:59:25 +0100

    MAR-919: Update the user attributes

--- api/user/management/commands/fix_bad_users_bugfix_mar919.py
+++ api/user/management/commands/fix_bad_users_bugfix_mar919.py
@@ -1,5 +1,40 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
 from django.db import transaction
+
+from api.barriers.models import Barrier, BarrierUserHit
+from api.collaboration.models import TeamMember
+from api.interactions.models import ExcludeFromNotification, Mention, TeamMember
+from api.user.models import (
+    MyBarriersSavedSearch,
+    Profile,
+    SavedSearch,
+    TeamBarriersSavedSearch,
+)
+from api.user_event_log.models import UserEvent
+
+User = settings.AUTH_USER_MODEL
+
+# These 5 functions have to be run in the same atomic DB operation
+
+
+def update_user_attribute(bad_user: User, good_user: User) -> None:
+    # Update the user attribute
+    TeamMember.objects.filter(user=bad_user).update(user=good_user)
+    BarrierUserHit.objects.filter(user=bad_user).update(user=good_user)
+    MyBarriersSavedSearch.objects.filter(user=bad_user).update(user=good_user)
+    Profile.objects.filter(user=bad_user).update(user=good_user)
+    SavedSearch.objects.filter(user=bad_user).update(user=good_user)
+    TeamBarriersSavedSearch.objects.filter(user=bad_user).update(user=good_user)
+    TeamMember.objects.filter(user=bad_user).update(user=good_user)
+    UserEvent.objects.filter(user=bad_user).update(user=good_user)
+
+    # Update misc User attributes
+    ExcludeFromNotification.objects.filter(excluded_user=bad_user).update(
+        uexcluded_user=good_user
+    )
+    Mention.objects.filter(recipient=bad_user).update(recipient=good_user)
 
 
 class Command(BaseCommand):
@@ -21,4 +56,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         with transaction.atomic():
-            pass
+            user_obj = get_user_model()
+            bad_user: User = user_obj.objects.get(id=options["bad_user_id"])
+            good_user: User = user_obj.objects.get(id=options["good_user_id"])
+
+            update_user_attribute(bad_user, good_user)
