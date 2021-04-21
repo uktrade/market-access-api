f6562105433034f98f0aa8c2dcadb00c274cd3b6
James Pacileo <jamespaxi@gmail.com>
2021-04-21 14:04:34 +0100

    datawarehouse api bugfix

--- api/barriers/models.py
+++ api/barriers/models.py
@@ -792,13 +792,11 @@ class PublicBarrier(FullyArchivableMixin, BaseModel):
         #
         # Note: cannot automatically change from published
         #       the public barrier would need to be unpublished first
-        PRE_PUBLISHING_STATUSES = [
-            PublicBarrierStatus.REVIEW_LATER,
-            PublicBarrierStatus.INELIGIBLE,
-            PublicBarrierStatus.ELIGIBLE,
-        ]
         if self._public_view_status != PublicBarrierStatus.PUBLISHED:
-            if self.barrier.public_eligibility_postponed is True:
+            if (
+                self.barrier.public_eligibility_postponed is True
+                and self._public_view_status != PublicBarrierStatus.REVIEW_LATER
+            ):
                 self._public_view_status = PublicBarrierStatus.REVIEW_LATER
                 self.save()
 
@@ -813,7 +811,7 @@ class PublicBarrier(FullyArchivableMixin, BaseModel):
             # Marking the public barrier eligible
             elif (
                 self.barrier.public_eligibility is True
-                and self._public_view_status in PRE_PUBLISHING_STATUSES
+                and self._public_view_status in [PublicBarrierStatus.INELIGIBLE, PublicBarrierStatus.REVIEW_LATER]
             ):
                 self._public_view_status = PublicBarrierStatus.ELIGIBLE
                 self.save()
