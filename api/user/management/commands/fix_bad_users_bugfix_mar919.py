e63c7b5a37e1f97c504a028a59930491a78bb9e0
Ciaran Doherty <cad106uk@gmail.com>
2021-05-05 11:15:39 +0100

    Mar 919 (#277)
    * MAR-919: start writing the management command to fix one user.
    
    * MAR-919: Update the user attributes
    
    * MAR-919: All tables to be updated
    
    * MAR-919: update all the base models (created_by, modified_by)
    
    * MAR-919: The achievable tables
    
    * MAR-919: The fully Achievable and approvable tables
    
    * MAR-919: setup of first unit test
    
    * MAR-919: Show one working command call
    
    * MAR-919: Remove the Profile object from the BugFix
    
    * MAR-919: Make the unit test consistently deal with 0 or 1 records
    
    * MAR-919: consistently handle how the 0 or 1 records
    
    These records are either updated or deleted. The TeamBarriersSavedSearch object
    still looks a little weird to me.
    
    * MAR-919: Only specific mention users are affected
    
    * MAR-919: Duplicate BarrierUserHit test
    
    This bug was found from running the command on a copy of the live data.
    
    * MAR-919: Deal with duplicate BarrierUserHit
    
    * MAR-919: delete the bad user after moving the data.
    
    * MAR-919: update username to be the current used email address
    
    * MAR-919: test to show good user record updating and the bad user deletion

---
+++ api/user/management/commands/fix_bad_users_bugfix_mar919.py
@@ -0,0 +1,199 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+from django.db import transaction
+from django.db.models import Q
+
+from api.assessment.models import (
+    EconomicAssessment,
+    EconomicImpactAssessment,
+    ResolvabilityAssessment,
+    StrategicAssessment,
+)
+from api.barriers.models import (
+    Barrier,
+    BarrierReportStage,
+    BarrierUserHit,
+    PublicBarrier,
+)
+from api.documents.models import Document
+from api.interactions.models import (
+    ExcludeFromNotification,
+    Interaction,
+    Mention,
+    PublicBarrierNote,
+    TeamMember,
+)
+from api.metadata.models import BarrierTag
+from api.user.models import MyBarriersSavedSearch, SavedSearch, TeamBarriersSavedSearch
+from api.user_event_log.models import UserEvent
+
+
+# These 5 functions have to be run in the same atomic DB operation
+def update_user_attribute(
+    bad_user: settings.AUTH_USER_MODEL, good_user: settings.AUTH_USER_MODEL
+) -> None:
+    # Update the user attribute
+    TeamMember.objects.filter(user=bad_user).update(user=good_user)
+    SavedSearch.objects.filter(user=bad_user).update(user=good_user)
+
+    UserEvent.objects.filter(user=bad_user).update(user=good_user)
+    Mention.objects.filter(recipient=bad_user).update(recipient=good_user)
+
+    # Explicitly delete one-to-one user relations for bad users.
+    if not BarrierUserHit.objects.filter(user=good_user).exists():
+        BarrierUserHit.objects.filter(user=bad_user).update(user=good_user)
+    if not MyBarriersSavedSearch.objects.filter(user=good_user).exists():
+        MyBarriersSavedSearch.objects.filter(user=bad_user).update(user=good_user)
+    if MyBarriersSavedSearch.objects.filter(user=bad_user).exists():
+        MyBarriersSavedSearch.objects.filter(user=bad_user).delete()
+    if not TeamBarriersSavedSearch.objects.filter(user=good_user).exists():
+        TeamBarriersSavedSearch.objects.filter(user=bad_user).update(user=good_user)
+    if TeamBarriersSavedSearch.objects.filter(user=bad_user).exists():
+        TeamBarriersSavedSearch.objects.filter(user=bad_user).delete()
+
+    # These are the misc User attributes. They are all one-to-one relations
+    if not ExcludeFromNotification.objects.filter(excluded_user=good_user).exists():
+        ExcludeFromNotification.objects.filter(excluded_user=bad_user).update(
+            excluded_user=good_user
+        )
+    if ExcludeFromNotification.objects.filter(
+        Q(excluded_user=bad_user) & ~Q(excluded_user=good_user)
+    ).exists():
+        ExcludeFromNotification.objects.filter(excluded_user=bad_user).delete()
+
+
+def update_basemodel_attributes(
+    bad_user: settings.AUTH_USER_MODEL, good_user: settings.AUTH_USER_MODEL
+) -> None:
+    # update the created_by attribute
+    Barrier.objects.filter(created_by=bad_user).update(created_by=good_user)
+    BarrierReportStage.objects.filter(created_by=bad_user).update(created_by=good_user)
+    BarrierTag.objects.filter(created_by=bad_user).update(created_by=good_user)
+    Document.objects.filter(created_by=bad_user).update(created_by=good_user)
+    EconomicAssessment.objects.filter(created_by=bad_user).update(created_by=good_user)
+    EconomicImpactAssessment.objects.filter(created_by=bad_user).update(
+        created_by=good_user
+    )
+    ExcludeFromNotification.objects.filter(created_by=bad_user).update(
+        created_by=good_user
+    )
+    Interaction.objects.filter(created_by=bad_user).update(created_by=good_user)
+    Mention.objects.filter(created_by=bad_user).update(created_by=good_user)
+    PublicBarrier.objects.filter(created_by=bad_user).update(created_by=good_user)
+    PublicBarrierNote.objects.filter(created_by=bad_user).update(created_by=good_user)
+    ResolvabilityAssessment.objects.filter(created_by=bad_user).update(
+        created_by=good_user
+    )
+    StrategicAssessment.objects.filter(created_by=bad_user).update(created_by=good_user)
+    TeamMember.objects.filter(created_by=bad_user).update(created_by=good_user)
+
+    # update the modified_by attribute
+    Barrier.objects.filter(modified_by=bad_user).update(modified_by=good_user)
+    BarrierReportStage.objects.filter(modified_by=bad_user).update(
+        modified_by=good_user
+    )
+    BarrierTag.objects.filter(modified_by=bad_user).update(modified_by=good_user)
+    Document.objects.filter(modified_by=bad_user).update(modified_by=good_user)
+    EconomicAssessment.objects.filter(modified_by=bad_user).update(
+        modified_by=good_user
+    )
+    EconomicImpactAssessment.objects.filter(modified_by=bad_user).update(
+        modified_by=good_user
+    )
+    ExcludeFromNotification.objects.filter(modified_by=bad_user).update(
+        modified_by=good_user
+    )
+    Interaction.objects.filter(modified_by=bad_user).update(modified_by=good_user)
+    Mention.objects.filter(modified_by=bad_user).update(modified_by=good_user)
+    PublicBarrier.objects.filter(modified_by=bad_user).update(modified_by=good_user)
+    PublicBarrierNote.objects.filter(modified_by=bad_user).update(modified_by=good_user)
+    ResolvabilityAssessment.objects.filter(modified_by=bad_user).update(
+        modified_by=good_user
+    )
+    StrategicAssessment.objects.filter(modified_by=bad_user).update(
+        modified_by=good_user
+    )
+    TeamMember.objects.filter(modified_by=bad_user).update(modified_by=good_user)
+
+
+def update_achivable_attributes(
+    bad_user: settings.AUTH_USER_MODEL, good_user: settings.AUTH_USER_MODEL
+) -> None:
+    # update the archived_by attribute
+    Barrier.objects.filter(archived_by=bad_user).update(archived_by=good_user)
+    Document.objects.filter(archived_by=bad_user).update(archived_by=good_user)
+    EconomicAssessment.objects.filter(archived_by=bad_user).update(
+        archived_by=good_user
+    )
+    EconomicImpactAssessment.objects.filter(archived_by=bad_user).update(
+        archived_by=good_user
+    )
+    Interaction.objects.filter(archived_by=bad_user).update(archived_by=good_user)
+    PublicBarrier.objects.filter(archived_by=bad_user).update(archived_by=good_user)
+    PublicBarrierNote.objects.filter(archived_by=bad_user).update(archived_by=good_user)
+    StrategicAssessment.objects.filter(archived_by=bad_user).update(
+        archived_by=good_user
+    )
+    TeamMember.objects.filter(archived_by=bad_user).update(archived_by=good_user)
+
+
+def update_fullyachivable_attributes(
+    bad_user: settings.AUTH_USER_MODEL, good_user: settings.AUTH_USER_MODEL
+) -> None:
+    # update the unarchived_by attribute
+    Barrier.objects.filter(unarchived_by=bad_user).update(unarchived_by=good_user)
+    PublicBarrier.objects.filter(unarchived_by=bad_user).update(unarchived_by=good_user)
+
+
+def update_approval_attributes(
+    bad_user: settings.AUTH_USER_MODEL, good_user: settings.AUTH_USER_MODEL
+) -> None:
+    # update the reviewed_by attribute
+    EconomicAssessment.objects.filter(reviewed_by=bad_user).update(
+        reviewed_by=good_user
+    )
+    StrategicAssessment.objects.filter(reviewed_by=bad_user).update(
+        reviewed_by=good_user
+    )
+    ResolvabilityAssessment.objects.filter(reviewed_by=bad_user).update(
+        reviewed_by=good_user
+    )
+
+
+class Command(BaseCommand):
+    """
+    https://uktrade.atlassian.net/browse/MAR-919
+
+    This is the fix to the missing mentions problem explained in ticket MAR-919
+    """
+
+    help = "this command will move all data from the bad user to the good user"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--bad_user_id", type=int, help="The rowId of the bad User record"
+        )
+        parser.add_argument(
+            "--good_user_id", type=int, help="The rowId of the good User record"
+        )
+
+    def handle(self, *args, **options):
+        with transaction.atomic():
+            user_obj = get_user_model()
+            bad_user: settings.AUTH_USER_MODEL = user_obj.objects.get(
+                id=options["bad_user_id"]
+            )
+            good_user: settings.AUTH_USER_MODEL = user_obj.objects.get(
+                id=options["good_user_id"]
+            )
+
+            update_user_attribute(bad_user, good_user)
+            update_basemodel_attributes(bad_user, good_user)
+            update_achivable_attributes(bad_user, good_user)
+            update_fullyachivable_attributes(bad_user, good_user)
+            update_approval_attributes(bad_user, good_user)
+
+            good_user.username = bad_user.username
+            bad_user.delete()
+            good_user.save()
