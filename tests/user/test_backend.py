3c5092d6a16c3be03124fd837ff87cd83e7d3a1d
Ciaran Doherty <cad106uk@gmail.com>
2021-05-10 12:10:45 +0100

    Mar 952 an authnentication broker (#286)
    * Add custom auth broker backend
    
    * MAR-952: Starting development. The plural is included in the singular
    
    * MAR-952: Clarify which profile objects are being referred to
    
    * MAR-952: First draft of authentication broker.
    
    * MAR-952: Upgrade django-staff-sso-client to 3.1.0
    
    * MAR-952: from PR Feedback remove is_staff and is_active
    
    * MAR-942: From PR feedback rename profile_obj to profile.
    
    I took the opportunity to put in type hinting to help me read what is happening
    in the code
    
    * MAR-952: From PR feedback make _build_profile a static method
    
    * MAR-952: The duplicate profile should throw an error not silently pass.
    
    * MAR-952: Basic unit tests with bug fixes.
    
    * MAR-952: add migration file
    
    * MAR-952: BugFix unit tests need to use the keyword assert to work.
    
    Co-authored-by: Ross Miller <github@decodedcollective.com>

---
+++ tests/user/test_backend.py
@@ -0,0 +1,68 @@
+import uuid
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from api.user.backend import CustomAuthbrokerBackend
+
+User = get_user_model()
+
+
+class TestCustomAuthbrokerBackend(TestCase):
+    def setUp(self):
+        user_id = uuid.uuid4()
+        self.mock_raw_profile = {
+            "user_id": user_id,
+            "email_user_id": f"first-{user_id}-last@trade.gov.uk",
+            "contact_email": "first-last@trade.gov.uk",
+            "email": "first-last@trade.gov.uk",
+            "first_name": "first",
+            "last_name": "last",
+        }
+
+    def test_build_profile(self):
+        user = User()
+        user.save()
+        profile_data = user.profile
+
+        assert profile_data.sso_user_id != self.mock_raw_profile["user_id"]
+        assert profile_data.sso_email_user_id != self.mock_raw_profile["email_user_id"]
+        assert profile_data.user.email != self.mock_raw_profile.get("contact_email")
+        # contact emails ?
+        assert (
+            profile_data.user.first_name != self.mock_raw_profile["first_name"]
+        )  # might change over time
+        assert (
+            profile_data.user.last_name != self.mock_raw_profile["last_name"]
+        )  # might change over time
+        assert profile_data.user.username != self.mock_raw_profile["email_user_id"]
+
+        CustomAuthbrokerBackend._build_profile(
+            profile_data, self.mock_raw_profile["email"], self.mock_raw_profile
+        )
+
+        assert profile_data.sso_user_id == self.mock_raw_profile["user_id"]
+        assert profile_data.sso_email_user_id == self.mock_raw_profile["email_user_id"]
+        assert profile_data.user.email == self.mock_raw_profile.get("contact_email")
+        # contact emails ?
+        assert (
+            profile_data.user.first_name == self.mock_raw_profile["first_name"]
+        )  # might change over time
+        assert (
+            profile_data.user.last_name == self.mock_raw_profile["last_name"]
+        )  # might change over time
+        assert profile_data.user.username == self.mock_raw_profile["email_user_id"]  #
+
+    def test_verify_user_object(self):
+        user = CustomAuthbrokerBackend.verify_user_object(self.mock_raw_profile)
+
+        assert (
+            user.first_name == self.mock_raw_profile["first_name"]
+        )  # might change over time
+        assert (
+            user.last_name == self.mock_raw_profile["last_name"]
+        )  # might change over time
+        assert user.username == self.mock_raw_profile["email_user_id"]
+        assert user.profile.sso_user_id == self.mock_raw_profile["user_id"]
+        assert user.profile.sso_email_user_id == self.mock_raw_profile["email_user_id"]
+        assert user.profile.user.email == self.mock_raw_profile.get("contact_email")
