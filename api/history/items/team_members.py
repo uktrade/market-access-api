f8d4e1b6894265f8254ea170ec4abbff552c3558
Ciaran Doherty <cad106uk@gmail.com>
2021-06-02 17:12:41 +0100

    MAR-962: commenting to explain what is happening.

--- api/history/items/team_members.py
+++ api/history/items/team_members.py
@@ -1,8 +1,14 @@
+"""
+From the fix for MAR-919 we have left 79 orphaned histories.
+To fix this, the orphaned histories are a set to have the default user (me)
+I can't help with any of histories but I can  stop the page crashing.
+"""
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ObjectDoesNotExist
 
 from .base import BaseHistoryItem
 
+# Getting my User object prepared
 backup_user = get_user_model().objects.get(email="ciaran.doherty@digital.trade.gov.uk")
 
 
@@ -16,8 +22,10 @@ class TeamMemberHistoryItem(BaseHistoryItem):
     def get_value(self, record):
         if record and not record.archived:
             try:
+                # Check that the user exists
                 user = getattr(record, "user", None)
             except ObjectDoesNotExist:
+                # default to me if user does not exist.
                 record.user = backup_user
                 record.save()
 
