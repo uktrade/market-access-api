d16aef3dc2471010a527fcb99a1f026e5ee27868
Ciaran Doherty <cad106uk@gmail.com>
2021-06-10 10:54:16 +0100

    debugging

--- utils/middleware.py
+++ utils/middleware.py
@@ -18,31 +18,37 @@ class SetAuthUserMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        keys = [
-            "COOKIES",
-            "META",
-            "_cached_user",
-            "_messages",
-            "_read_started",
-            "_stream",
-            "content_params",
-            "content_type",
-            "environ",
-            "method",
-            "path",
-            "path_info",
-            "resolver_match",
-            "session",
-            "user",
-        ]
-        for key in keys:
-            logging.warning(f"STUB: |{key}| |{getattr(request, key)}|")
+        keys = "".join([f"|key|" for keys in request.session.__dict__.keys()])
+        logging.warning(f"STUB: session KEYS {keys}")
+        keys = "".join([f"|key|" for keys in request._stream.__dict__.keys()])
+        logging.warning(f"STUB: _stream KEYS {keys}")
+        keys = "".join([f"|key|" for keys in request._messages.__dict__.keys()])
+        logging.warning(f"STUB: _messages KEYS {keys}")
+        # keys = [
+        #     "environ",
+        #     "path_info",
+        #     "path",
+        #     "META",
+        #     "method",
+        #     "content_type",
+        #     "content_params",
+        #     "_stream",
+        #     "_read_started",
+        #     "resolver_match",
+        #     "COOKIES",
+        #     "session",
+        #     "user",
+        #     "_messages",
+        #     "_cached_user",
+        # ]
+        # for key in keys:
+        #     logging.warning(f"STUB: |{key}| |{getattr(request, key)}|")
 
-        token = request.environ["HTTP_AUTHORIZATION"].split(" ")[-1]
-        logging.warning(f"STUB:token {token}")
-        try:
-            user = UserModel.objects.get(profile__sso_user_id=token)
-            logging.warning(f"STUB:user {user}")
-        except Exception as exc:
-            logging.warning(f"STUB ERROR {exc}")
+        # token = request.environ["HTTP_AUTHORIZATION"].split(" ")[-1]
+        # logging.warning(f"STUB:token {token}")
+        # try:
+        #     user = UserModel.objects.get(profile__sso_user_id=token)
+        #     logging.warning(f"STUB:user {user}")
+        # except Exception as exc:
+        #     logging.warning(f"STUB ERROR {exc}")
         return self.get_response(request)
