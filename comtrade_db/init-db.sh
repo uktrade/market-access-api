4dde502484f12ccb6adee0e1c02aa4764af2b7ef
Ciaran Doherty <cad106uk@gmail.com>
2021-07-28 11:50:50 +0100

    backup save of docker changes.

--- comtrade_db/init-db.sh
+++ comtrade_db/init-db.sh
@@ -6,4 +6,5 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     GRANT ALL PRIVILEGES ON DATABASE comtrade TO comtrade_user;
 EOSQL
 
-psql -f /db-dumps/db.dump.sql newdb
+psql -f /db-dumps/db.dump.sql comtrade;
+
