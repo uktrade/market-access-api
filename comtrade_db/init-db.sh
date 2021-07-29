1de2789d2fea2c43a802f5c07875918d8ada6d19
Ciaran Doherty <cad106uk@gmail.com>
2021-07-29 12:24:11 +0100

    remove duplicated create user command

--- comtrade_db/init-db.sh
+++ comtrade_db/init-db.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER comtrade_user;
     CREATE DATABASE comtrade;
     GRANT ALL PRIVILEGES ON DATABASE comtrade TO comtrade_user;
 EOSQL
