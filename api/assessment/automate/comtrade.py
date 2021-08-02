e5e1652748fe99eae9465f0479ed44b4e79b3557
Kevin Carrogan <kevin.carrogan@gmail.com>
2021-08-02 15:23:22 +0100

    Fix incorrect placeholder variable SQL syntax

--- api/assessment/automate/comtrade.py
+++ api/assessment/automate/comtrade.py
@@ -68,7 +68,7 @@ class ComtradeClient:
 
         query: sql.SQL = sql.SQL(
             "SELECT *, 'TOTAL' as commodity_code FROM comtrade__goods WHERE "
-            "period IN ? AND trade_flow_code IN ? partner_code IN ? AND reporter_code IN ?"
+            "period IN %s AND trade_flow_code IN %s AND partner_code IN %s AND reporter_code IN %s"
         )
         with connections["comtrade"].cursor() as cur:
             cur.execute(query, [period, trade_flow_code, partner_code, reporter_code])
