6026cdaf32dfe2ef29da3c3ff9bfd9f7dfe99d46
James Pacileo <james.pacileo@digital.trade.gov.uk>
2021-10-29 15:46:47 +0100

    MAR-1049

--- api/commodities/management/commands/import_commodities.py
+++ api/commodities/management/commands/import_commodities.py
@@ -13,8 +13,8 @@ class Command(BaseCommand):
         parser.add_argument("--version-date", type=str, help="Version (ISO-8601 date)")
 
     def handle(self, *args, **options):
-        self.import_commodities(options["file"], options["version-date"])
-        self.add_parents(options["file"], options["version-date"])
+        self.import_commodities(options["file"], options["version_date"])
+        self.add_parents(options["file"], options["version_date"])
 
     def import_commodities(self, csv_file, version):
         self.stdout.write("Importing commodities...")
@@ -23,17 +23,20 @@ class Command(BaseCommand):
             reader = csv.DictReader(file)
             i = 0
             for line in reader:
-                commodity = Commodity.objects.create(
-                    version=version,
-                    code=line["Commodity code"],
-                    suffix=line["Suffix"],
-                    level=line["HS Level"],
-                    indent=line["Indent"],
-                    description=line["Description"],
-                    is_leaf=(line["Suffix"] == "80"),
-                    sid=line["SID"],
-                    parent_sid=line["Parent SID"] or None,
-                )
+                padded_code = line["Commodity code"].ljust(10, "0")
+                if Commodity.objects.filter(code=padded_code).count() == 0:
+                    commodity = Commodity.objects.create(
+                        version=version,
+                        code=line["Commodity code"].ljust(10, "0"),
+                        # suffix=line["Suffix"],
+                        # level=line["HS Level"],
+                        # indent=line["Indent"],
+                        description=line["Description"],
+                        is_leaf=(line["Suffix"] == "80"),
+                        sid=line["SID"],
+                        parent_sid=line["Parent SID"] or None,
+                        classification="H4",
+                    )
 
                 if i % 1000 == 0:
                     self.stdout.write(str(i))
--- api/commodities/models.py
+++ api/commodities/models.py
@@ -6,14 +6,15 @@ from .utils import format_commodity_code
 class Commodity(models.Model):
     version = models.CharField(max_length=32, db_index=True)
     code = models.CharField(max_length=10, db_index=True)
-    suffix = models.CharField(max_length=2)
+    suffix = models.CharField(max_length=2, null=True)
     description = models.TextField()
-    level = models.IntegerField()
-    indent = models.IntegerField()
+    level = models.IntegerField(null=True)
+    indent = models.IntegerField(null=True)
     parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True)
     is_leaf = models.BooleanField(default=False, db_index=True)
-    sid = models.IntegerField()
+    sid = models.IntegerField(null=True)
     parent_sid = models.IntegerField(null=True)
+    classification = models.CharField(default="H5", max_length=10)
 
     @property
     def full_description(self):
