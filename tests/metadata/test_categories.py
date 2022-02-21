3dfe7f6db0c0ccf2bd6f874f1d09f7ac5f159680
James Pacileo <jamespacileo@users.noreply.github.com>
2022-02-21 11:15:32 +0000

    TSS-106 - test update

--- tests/metadata/test_categories.py
+++ tests/metadata/test_categories.py
@@ -5,11 +5,11 @@ from api.metadata.models import Category
 class TestCategories(APITestMixin):
     def test_categories_count(self):
         categories_count = Category.objects.count()
-        assert categories_count == 33
+        assert categories_count == 34
 
     def test_categories_goods_count(self):
         categories_count = Category.goods.count()
-        assert categories_count == 26
+        assert categories_count == 27
 
     def test_categories_services_count(self):
         categories_count = Category.services.count()
