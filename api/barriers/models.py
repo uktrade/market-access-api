820f4c9cdca2d61f39ea9be2f099d40dc37ed399
James Pacileo <608868+jamespacileo@users.noreply.github.com>
2022-03-18 13:42:27 +0000

    TSS-106

--- api/barriers/models.py
+++ api/barriers/models.py
@@ -215,6 +215,8 @@ class BarrierHistoricalModel(models.Model):
 
 class BarrierProgressUpdate(FullyArchivableMixin, BaseModel):
     id = models.UUIDField(primary_key=True, default=uuid4)
+    created_on = models.DateTimeField(db_index=True, null=True, blank=True)
+    modified_on = models.DateTimeField(null=True, blank=True)
     barrier = models.ForeignKey(
         "Barrier", on_delete=models.CASCADE, related_name="progress_updates"
     )
@@ -229,6 +231,15 @@ class BarrierProgressUpdate(FullyArchivableMixin, BaseModel):
         blank=True,
         null=True,
     )
+
+    def save(self, *args, **kwargs):
+        # timestamp needs to be identical for both created and modified
+        # auto_now and auto_now_add create different datetime values
+        now = timezone.now()
+        if not self.pk:
+            kwargs["created_on"] = now
+        kwargs["modified_on"] = now
+        super().save(*args, **kwargs)
 
     class Meta:
         # order by date descending
--- api/barriers/views.py
+++ api/barriers/views.py
@@ -1,6 +1,5 @@
 import csv
 from collections import defaultdict
-from datetime import datetime
 from itertools import chain
 
 from dateutil.parser import parse
@@ -885,12 +884,9 @@ class BarrierProgressUpdateViewSet(ModelViewSet):
 
     def perform_create(self, serializer, user):
         # share the exact same date within both created_on and modified_on
-        now = datetime.now()
         instance = serializer.save()
         instance.created_by = user
-        instance.created_on = now
         instance.modified_by = user
-        instance.modified_on = now
         return instance.save()
 
     def create(self, request, *args, **kwargs):
@@ -903,10 +899,8 @@ class BarrierProgressUpdateViewSet(ModelViewSet):
         )
 
     def perform_update(self, serializer, user):
-        now = datetime.now()
         instance = serializer.save()
         instance.modified_by = user
-        instance.modified_on = now
         return instance.save()
 
     def update(self, request, *args, **kwargs):
