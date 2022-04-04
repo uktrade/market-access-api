abf2690926f33b88c9c92c312d58c96cc386ddbc
James Pacileo <608868+jamespacileo@users.noreply.github.com>
2022-04-04 13:21:14 +0100

    TSS-146

--- tests/barriers/test_inactivity_reminders.py
+++ tests/barriers/test_inactivity_reminders.py
@@ -71,62 +71,62 @@ class TestBarrierInactivityReminders(TestCase):
                 == 10
             )
 
-    # def test_individual_notification(self):
-    #     test_user = create_test_user()
-    #     inactivity_theshold_date = datetime.now() - timedelta(
-    #         days=settings.BARRIER_INACTIVITY_THESHOLD_DAYS
-    #     )
-    #     repeat_reminder_theshold_date = datetime.now() - timedelta(
-    #         days=settings.BARRIER_REPEAT_REMINDER_THESHOLD_DAYS
-    #     )
-    #     #  test individual call params
-    #     test_barrier = BarrierFactory()
-    #     TeamMemberFactory(
-    #         barrier=test_barrier, user=test_user, role="Owner", default=True
-    #     )
-    #     Barrier.objects.filter(id=test_barrier.id).update(
-    #         modified_on=inactivity_theshold_date - timedelta(days=1),
-    #         activity_reminder_sent=None,
-    #     )
+    def test_individual_notification(self):
+        test_user = create_test_user()
+        inactivity_theshold_date = datetime.now() - timedelta(
+            days=settings.BARRIER_INACTIVITY_THESHOLD_DAYS
+        )
+        repeat_reminder_theshold_date = datetime.now() - timedelta(
+            days=settings.BARRIER_REPEAT_REMINDER_THESHOLD_DAYS
+        )
+        #  test individual call params
+        test_barrier = BarrierFactory()
+        TeamMemberFactory(
+            barrier=test_barrier, user=test_user, role="Owner", default=True
+        )
+        Barrier.objects.filter(id=test_barrier.id).update(
+            modified_on=inactivity_theshold_date - timedelta(days=1),
+            activity_reminder_sent=None,
+        )
 
-    #     assert (
-    #         test_barrier.id
-    #         == Barrier.objects.filter(modified_on__lt=inactivity_theshold_date)
-    #         .filter(
-    #             Q(activity_reminder_sent__isnull=True)
-    #             | Q(activity_reminder_sent__lt=repeat_reminder_theshold_date)
-    #         )
-    #         .first()
-    #         .id
-    #     )
+        assert (
+            test_barrier.id
+            == Barrier.objects.filter(modified_on__lt=inactivity_theshold_date)
+            .filter(
+                Q(activity_reminder_sent__isnull=True)
+                | Q(activity_reminder_sent__lt=repeat_reminder_theshold_date)
+            )
+            .first()
+            .id
+        )
 
-    #     assert (
-    #         Barrier.objects.filter(modified_on__lt=inactivity_theshold_date)
-    #         .filter(
-    #             Q(activity_reminder_sent__isnull=True)
-    #             | Q(activity_reminder_sent__lt=repeat_reminder_theshold_date)
-    #         )
-    #         .count()
-    #         == 1
-    #     )
+        assert (
+            Barrier.objects.filter(modified_on__lt=inactivity_theshold_date)
+            .filter(
+                Q(activity_reminder_sent__isnull=True)
+                | Q(activity_reminder_sent__lt=repeat_reminder_theshold_date)
+            )
+            .count()
+            == 1
+        )
 
-    #     with patch.object(NotificationsAPIClient, "send_email_notification") as mock:
-    #         owner = test_barrier.barrier_team.get(role="Owner").user
+        with patch.object(NotificationsAPIClient, "send_email_notification") as mock:
+            owner = test_barrier.barrier_team.get(role="Owner").user
 
-    #         assert owner.email == test_user.email
+            assert owner.email == test_user.email
 
-    #         send_barrier_inactivity_reminders()
+            send_barrier_inactivity_reminders()
 
-    #         email_personalisation = {
-    #             "barrier_title": test_barrier.title,
-    #             "barrier_url": f"{settings.DMAS_BASE_URL}/barriers/{test_barrier.id}/",
-    #             "full_name": f"{owner.first_name} {owner.last_name}",
-    #             "barrier_created_date": test_barrier.created_on.strftime("%d %B %Y"),
-    #         }
+            email_personalisation = {
+                "barrier_title": test_barrier.title,
+                "barrier_url": f"{settings.DMAS_BASE_URL}/barriers/{test_barrier.id}/",
+                "full_name": f"{owner.first_name} {owner.last_name}",
+                "barrier_created_date": test_barrier.created_on.strftime("%d %B %Y"),
+            }
 
-    #         assert mock.call_count == 1
-    #         mock.assert_called_once_with(
-    #             email_address=owner.email,
-    #             template_id=settings.BARRIER_INACTIVITY_REMINDER_NOTIFICATION_ID,
-    #             personalisation=email_personalisation,
-    #         )
+            assert mock.call_count == 1
+            mock.assert_called_once_with(
+                email_address=owner.email,
+                template_id=settings.BARRIER_INACTIVITY_REMINDER_NOTIFICATION_ID,
+                personalisation=email_personalisation,
+            )
