fdfe779f96a26609cfd8ee6ee5d62b67fa137f02
Nick Fitzsimons <nick@nickfitz.co.uk>
2022-07-13 17:31:10 +0100

    Stakeholders for action plan tasks.

--- api/action_plans/models.py
+++ api/action_plans/models.py
@@ -1,6 +1,7 @@
 from uuid import uuid4
 
 from django.contrib.auth import get_user_model
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.db.models.signals import post_save
 from django.utils import timezone
@@ -109,7 +110,11 @@ class ActionPlanTask(models.Model):
         User, null=True, blank=True, on_delete=models.SET_NULL
     )
 
-    stakeholders = models.TextField(default="", blank=True)
+    assigned_stakeholders = ArrayField(
+        models.UUIDField(),
+        blank=True,
+        default=list,
+    )
     outcome = models.TextField(default="", blank=True)
     progress = models.TextField(default="", blank=True)
 
