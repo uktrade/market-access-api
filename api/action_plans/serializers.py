c4db4fa6d5146b61ec848085e29a67e627f75f59
James Pacileo <608868+jamespacileo@users.noreply.github.com>
2022-07-22 17:03:39 +0100

    TSS-299 (#412)
    * Choices for stakeholder status.
    
    * Stakeholder model.
    
    * Tests for creating and updating action plan stakeholders.
    
    * Stakeholder create and update views.
    
    * Include the action plan in the barrier serialisation.
    
    * Stakeholders for action plan tasks.
    
    * Serialisation of action plan and action plan task stakeholders.
    
    * Allow deletion of action plan stakeholders.
    
    * TSS-299
    
    * TSS-299
    
    * Fix validation error causing failing barrier detail tests.
    
    * Forgot about these action plan tests.
    
    * Remove commented-out cruft.
    
    * TSS-299
    
    Co-authored-by: Nick Fitzsimons <nick@nickfitz.co.uk>

--- api/action_plans/serializers.py
+++ api/action_plans/serializers.py
@@ -1,10 +1,33 @@
 from rest_framework import serializers
 
-from api.action_plans.models import ActionPlan, ActionPlanMilestone, ActionPlanTask
+from api.action_plans.models import (
+    ActionPlan,
+    ActionPlanMilestone,
+    ActionPlanTask,
+    Stakeholder,
+)
+
+
+class ActionPlanStakeholderSerializer(serializers.ModelSerializer):
+    from rest_framework.fields import empty
+
+    def run_validation(self, data=empty):
+        return super().run_validation(data)
+
+    class Meta:
+        model = Stakeholder
+        fields = (
+            "id",
+            "action_plan",
+            "name",
+            "status",
+            "organisation",
+            "job_title",
+            "is_organisation",
+        )
 
 
 class ActionPlanTaskSerializer(serializers.ModelSerializer):
-
     assigned_to_email = serializers.SerializerMethodField()
     action_type_display = serializers.SerializerMethodField()
 
@@ -13,13 +36,14 @@ class ActionPlanTaskSerializer(serializers.ModelSerializer):
         fields = (
             "id",
             "milestone",
+            "action_text",
             "status",
             "start_date",
             "completion_date",
             "action_text",
             "action_type",
             "action_type_category",
-            "stakeholders",
+            "assigned_stakeholders",
             "action_type_display",
             "assigned_to",
             "assigned_to_email",
@@ -45,6 +69,12 @@ class ActionPlanMilestoneSerializer(serializers.ModelSerializer):
 
     tasks = ActionPlanTaskSerializer(many=True, required=False, read_only=True)
 
+    def create(self, validated_data):
+        return super().create(validated_data)
+
+    def to_representation(self, instance):
+        return super().to_representation(instance)
+
     class Meta:
         model = ActionPlanMilestone
         fields = ("id", "action_plan", "objective", "completion_date", "tasks")
@@ -54,6 +84,7 @@ class ActionPlanSerializer(serializers.ModelSerializer):
 
     milestones = ActionPlanMilestoneSerializer(many=True)
     owner_email = serializers.SerializerMethodField()
+    stakeholders = ActionPlanStakeholderSerializer(many=True, read_only=True)
 
     class Meta:
         model = ActionPlan
@@ -68,6 +99,13 @@ class ActionPlanSerializer(serializers.ModelSerializer):
             "status",
             "strategic_context",
             "strategic_context_last_updated",
+            # risks and mitigations fields
+            "potential_unwanted_outcomes",
+            "potential_risks",
+            "risk_level",
+            "risk_mitigation_measures",
+            # stakeholders fields
+            "stakeholders",
         )
         lookup_field = "barrier"
 
