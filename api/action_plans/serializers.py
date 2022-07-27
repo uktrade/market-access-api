5cc67f45501e7c8049897001ade519b85848f21d
Nick Fitzsimons <nick@nickfitz.co.uk>
2022-07-27 13:47:35 +0100

    Remove leftover debug cruft.

--- api/action_plans/serializers.py
+++ api/action_plans/serializers.py
@@ -69,12 +69,6 @@ class ActionPlanMilestoneSerializer(serializers.ModelSerializer):
 
     tasks = ActionPlanTaskSerializer(many=True, required=False, read_only=True)
 
-    def create(self, validated_data):
-        return super().create(validated_data)
-
-    def to_representation(self, instance):
-        return super().to_representation(instance)
-
     class Meta:
         model = ActionPlanMilestone
         fields = ("id", "action_plan", "objective", "completion_date", "tasks")
