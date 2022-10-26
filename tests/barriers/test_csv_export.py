184c9ee47c6d6797d04ef16dbdcf98c124f30ed7
Nick Fitzsimons <nick@nickfitz.co.uk>
2022-10-26 13:59:37 +0100

    Tests for resolved top priority status in CSV download.

--- tests/barriers/test_csv_export.py
+++ tests/barriers/test_csv_export.py
@@ -193,6 +193,60 @@ class TestBarrierCsvExportSerializer(APITestMixin, APITestCase):
             assert "priority_summary" in serialised_data
             assert serialised_data["priority_summary"] == expected_priority_summary
 
+    def test_has_value_for_is_resolved_top_priority(self):
+        barrier = BarrierFactory(status_date=datetime.date.today())
+        serialised_data = BarrierCsvExportSerializer(barrier).data
+        assert "is_resolved_top_priority" in serialised_data.keys()
+
+    def test_value_for_is_resolved_top_priority_is_bool(self):
+        barrier = BarrierFactory(status_date=datetime.date.today())
+        serialised_data = BarrierCsvExportSerializer(barrier).data
+        assert isinstance(serialised_data["is_resolved_top_priority"], bool)
+
+    def test_is_resolved_top_priority_value_for_resolved_top_priority_is_correct(self):
+        barrier = BarrierFactory(
+            status_date=datetime.date.today(),
+            top_priority_status=TOP_PRIORITY_BARRIER_STATUS.RESOLVED,
+        )
+        serialised_data = BarrierCsvExportSerializer(barrier).data
+        assert serialised_data["is_resolved_top_priority"] is True
+
+    def test_is_resolved_top_priority_value_for_approved_top_priority_is_correct(self):
+        barrier = BarrierFactory(
+            status_date=datetime.date.today(),
+            top_priority_status=TOP_PRIORITY_BARRIER_STATUS.APPROVED,
+        )
+        serialised_data = BarrierCsvExportSerializer(barrier).data
+        assert serialised_data["is_resolved_top_priority"] is False
+
+    def test_is_resolved_top_priority_value_for_approval_pending_top_priority_is_correct(
+        self,
+    ):
+        barrier = BarrierFactory(
+            status_date=datetime.date.today(),
+            top_priority_status=TOP_PRIORITY_BARRIER_STATUS.APPROVAL_PENDING,
+        )
+        serialised_data = BarrierCsvExportSerializer(barrier).data
+        assert serialised_data["is_resolved_top_priority"] is False
+
+    def test_is_resolved_top_priority_value_for_removal_pending_top_priority_is_correct(
+        self,
+    ):
+        barrier = BarrierFactory(
+            status_date=datetime.date.today(),
+            top_priority_status=TOP_PRIORITY_BARRIER_STATUS.REMOVAL_PENDING,
+        )
+        serialised_data = BarrierCsvExportSerializer(barrier).data
+        assert serialised_data["is_resolved_top_priority"] is False
+
+    def test_is_resolved_top_priority_value_for_no_top_priority_is_correct(self):
+        barrier = BarrierFactory(
+            status_date=datetime.date.today(),
+            top_priority_status=TOP_PRIORITY_BARRIER_STATUS.NONE,
+        )
+        serialised_data = BarrierCsvExportSerializer(barrier).data
+        assert serialised_data["is_resolved_top_priority"] is False
+
     def test_valuation_assessment_midpoint(self):
         impact_level = 6
         barrier = BarrierFactory()
