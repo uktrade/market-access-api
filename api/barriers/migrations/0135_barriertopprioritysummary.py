273ae7b577db0ef95fd451b99f854d1558f1a0b0
santinomolinaro <santino.molinaro@mail>
2022-11-28 09:28:36 +0000

    Rename migration after rebasing

--- api/barriers/migrations/0135_barriertopprioritysummary.py
+++ api/barriers/migrations/0135_barriertopprioritysummary.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('barriers', '0132_auto_20221021_1052'),
+        ('barriers', '0134_migrate_unknown_barriers_to_open'),
     ]
 
     operations = [
