6798af9afb3a62f320cb7f62dc00ef0266456e8d
ferozerub <feroze@taridigital.com>
2023-01-26 10:02:02 +0000

    Clean up imports

--- api/user/views.py
+++ api/user/views.py
@@ -1,8 +1,8 @@
 from http import HTTPStatus
 
-from django.conf import settings
 from hawkrest import HawkAuthentication
 
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 from django.db.models import F
