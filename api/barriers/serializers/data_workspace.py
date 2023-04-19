d157634a54bbbd0f8ccb0288266e871c25e4e4b3
santinomolinaro <santino.molinaro@mail>
2023-04-19 13:33:01 +0100

    Changing format of resolution_date in csv and data workspace

--- api/barriers/serializers/data_workspace.py
+++ api/barriers/serializers/data_workspace.py
@@ -287,7 +287,7 @@ class DataWorkspaceSerializer(AssessmentFieldsMixin, BarrierSerializerBase):
 
     def get_resolved_date(self, obj):
         if obj.status_date and (obj.status == 4 or obj.status == 3):
-            return obj.status_date
+            return obj.status_date.strftime("%d-%b-%y")
         else:
             return None
 
