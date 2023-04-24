cb723d91d9e5ad8be25360804ba708d90be14726
Santino Molinaro <85284133+Santino-Trade@users.noreply.github.com>
2023-04-24 15:24:29 +0100

    Changing format of resolution_date in csv and data workspace (#515)
    * Changing format of resolution_date in csv and data workspace
    
    * Updating unit tests to reflect new date format
    
    ---------
    
    Co-authored-by: santinomolinaro <santino.molinaro@mail>

--- api/barriers/serializers/csv.py
+++ api/barriers/serializers/csv.py
@@ -205,7 +205,7 @@ class BarrierCsvExportSerializer(AssessmentFieldsMixin, serializers.Serializer):
 
     def get_resolved_date(self, obj):
         if obj.status_date and (obj.status == 4 or obj.status == 3):
-            return obj.status_date.strftime("%m/%Y")
+            return obj.status_date.strftime("%d-%b-%y")
         else:
             return None
 
