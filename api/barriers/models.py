5f74202dba7abaea7bd7f5a410d96f8d6ad470c1
Uka Osim <hsojo91@gmail.com>
2023-05-16 15:14:54 +0100

    fixing method property

--- api/barriers/models.py
+++ api/barriers/models.py
@@ -521,8 +521,8 @@ class Barrier(FullyArchivableMixin, BaseModel):
 
     @property
     def latest_programme_fund_progress_update(self):
-        if self.programme_fund_progress_updates.all().exists():
-            return self.programme_fund_progress_updates.all().latest("created_on")
+        if self.programme_fund_progress_updates.exists():
+            return self.programme_fund_progress_updates.latest("created_on")
         return None
 
     @property
