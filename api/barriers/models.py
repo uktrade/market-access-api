4a620c3091274fa31a434f4a88acd3094f7beabc
Uka Osim <hsojo91@gmail.com>
2023-06-20 16:58:43 +0100

    wip

--- api/barriers/models.py
+++ api/barriers/models.py
@@ -496,10 +496,10 @@ class Barrier(FullyArchivableMixin, BaseModel):
 
     start_date = models.DateField(blank=True, null=True)
 
-    export_types = models.ManyToManyField(
-        metadata_models.ExportType,
-        blank=True,
-    )
+    # export_types = models.ManyToManyField(
+    #     metadata_models.ExportType,
+    #     blank=True,
+    # )
 
     def __str__(self):
         if self.title is None:
