58947ff464340dbb163fb9d17c9f23f9f3e0f9d3
Uka Osim <hsojo91@gmail.com>
2023-06-20 15:32:20 +0100

    wip

--- api/barriers/models.py
+++ api/barriers/models.py
@@ -482,10 +482,6 @@ class Barrier(FullyArchivableMixin, BaseModel):
         metadata_models.Organisation,
         help_text="Organisations that are related to the barrier",
     )
-    # export_types = models.ManyToManyField(
-    #     metadata_models.ExportType,
-    #     blank=True,
-    # )
 
     history = HistoricalRecords(bases=[BarrierHistoricalModel])
 
@@ -499,6 +495,11 @@ class Barrier(FullyArchivableMixin, BaseModel):
     )
 
     start_date = models.DateField(blank=True, null=True)
+
+    export_types = models.ManyToManyField(
+        metadata_models.ExportType,
+        blank=True,
+    )
 
     def __str__(self):
         if self.title is None:
