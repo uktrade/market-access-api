3fab9c748143ce9962fb0d8247e1fe0c39bef178
Feroze Rub <feroze@taridigital.com>
2023-06-29 15:36:29 +0100

    Test iSort

--- api/feedback/views.py
+++ api/feedback/views.py
@@ -1,7 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
-
-from api.feedback.serializers import FeedbackSerializer
 from .models import Feedback
+from api.feedback.serializers import FeedbackSerializer
 
 
 class AddFeedbackViewSet(ModelViewSet):
