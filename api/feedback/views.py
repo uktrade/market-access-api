6f49eb73d932c705161c9232f8e3aefce52f1da3
Feroze Rub <feroze@taridigital.com>
2023-06-29 15:06:19 +0100

    Allow feedback to capture CSAT rating and then updated with comments

--- api/feedback/views.py
+++ api/feedback/views.py
@@ -1,11 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
 
 from api.feedback.serializers import FeedbackSerializer
+from .models import Feedback
 
 
 class AddFeedbackViewSet(ModelViewSet):
     serializer_class = FeedbackSerializer
-    allowed_methods = ("post",)
+    queryset = Feedback.objects.all()
 
     def create(self, request, *args, **kwargs):
         return super().create(request, *args, **kwargs)
