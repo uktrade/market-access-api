997fdf9fbd221cd899da493128af4973f1d29800
Feroze Rub <feroze@taridigital.com>
2023-08-02 17:33:43 +0100

    Fix default values for First Name, Last Name where do not exist

--- api/barriers/serializers/data_workspace.py
+++ api/barriers/serializers/data_workspace.py
@@ -394,8 +394,8 @@ class DataWorkspaceSerializer(AssessmentFieldsMixin, BarrierSerializerBase):
     def get_programme_fund_progress_update_author(self, instance):
         if instance.latest_programme_fund_progress_update:
             author = instance.latest_programme_fund_progress_update.created_by
-            first_name = getattr(author, "first_name")
-            last_name = getattr(author, "last_name")
+            first_name = getattr(author, "first_name", None)
+            last_name = getattr(author, "last_name", None)
             return f"{first_name} {last_name}" if first_name and last_name else None
 
     def get_programme_fund_progress_update_date(self, instance):
