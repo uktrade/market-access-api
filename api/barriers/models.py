084b06c9fc4f8b809dfb0846ecc58e055767a27f
Uka Osim <hsojo91@gmail.com>
2023-08-07 21:34:58 +0100

    add check main sector

--- api/barriers/models.py
+++ api/barriers/models.py
@@ -1393,8 +1393,8 @@ class BarrierFilterSet(django_filters.FilterSet):
         custom filter for multi-select filtering of Sectors field,
         which is ArrayField
         """
-        return queryset.filter(
-            Q(all_sectors=True) | Q(sectors__overlap=value) | Q(main_sector__in=value)
+        return queryset.filter(Q(main_sector__in=value)).filter(
+            Q(all_sectors=True) | Q(sectors__overlap=value)
         )
 
     def ignore_all_sectors_filter(self, queryset, name, value):
