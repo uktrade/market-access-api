e1e2671ff76384a1d484a69387e6b2d104ffb729
christopherpettinga <chrispettinga@gmail.com>
2023-08-29 12:24:27 +0100

    Removing auditlog middleware for local development as it clogs up the logging output.
    Also updated pre-commit.sh to ignore docker commands if docker is not being used, denoted by the IGNORE_DOCKER env variable

--- config/settings/local.py
+++ config/settings/local.py
@@ -12,4 +12,6 @@ CELERY_TASK_ALWAYS_EAGER = True
 BROKER_URL = "redis://redis:6379/2"
 CELERY_RESULT_BACKEND = "redis://redis:6379/3"
 
-LOGGING = DEFAULT_LOGGING
+LOGGING = DEFAULT_LOGGING  # we don't care about comprehensive ECS logging for local
+
+MIDDLEWARE.remove("django_audit_log_middleware.AuditLogMiddleware")  # we don't care about audit logs for local
