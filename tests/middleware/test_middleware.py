616bd80f766d31f6cc51acca6585a4fd29358e6f
christopherpettinga <chrispettinga@gmail.com>
2023-09-11 22:37:17 +0100

    added new middleware to add user context to all messages sent to Sentry, making debugger a lot easier

---
+++ tests/middleware/test_middleware.py
@@ -0,0 +1,39 @@
+from unittest.mock import Mock
+
+from django.test import TestCase
+from sentry_sdk.hub import Hub
+
+from api.core.middleware import SentryUserContextMiddleware
+
+
+class TestMiddleware(TestCase):
+
+    """
+    Test Healthcheck Middleware
+    """
+
+    def setUp(self):
+        super().setUp()
+        self.middleware = SentryUserContextMiddleware(Mock())
+        self.request = Mock()
+
+
+    def test_middleware_authenticated_user_sentry_context_added(self):
+        assert Hub.current.scope._user is None
+        mock_user = Mock()
+        mock_user.id = 1
+        mock_user.email = "test@example.com"
+        self.mock_user = mock_user
+        self.request.user = mock_user
+        self.middleware(self.request)
+        assert Hub.current.scope._user
+        assert Hub.current.scope._user == {
+            "id": str(mock_user.id),
+            "email": mock_user.email,
+        }
+
+    def test_middleware_unauthenticated_user_sentry_context_added(self):
+        self.request.user.is_authenticated = False
+        assert Hub.current.scope._user is None
+        self.middleware(self.request)
+        assert Hub.current.scope._user is None
