4bac310800566e0c63f729a141d701ea261bf911
Uka Osim <hsojo91@gmail.com>
2023-09-21 09:46:38 +0100

    ignore imports flake8

--- api/barriers/serializers/__init__.py
+++ api/barriers/serializers/__init__.py
@@ -1,8 +1,8 @@
 from .barriers import (
-    BarrierDetailSerializer,
-    BarrierListSerializer,
-    BarrierSlimListSerializer,
-)  # noqa
+    BarrierDetailSerializer,  # noqa
+    BarrierListSerializer,  # noqa
+    BarrierSlimListSerializer,  # noqa
+)
 from .csv import BarrierCsvExportSerializer  # noqa
 from .data_workspace import DataWorkspaceSerializer  # noqa
 from .progress_updates import ProgressUpdateSerializer  # noqa
