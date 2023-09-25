891bfc32bac7f4a14f5cfa03843afd7827617530
christopherpettinga <chrispettinga@gmail.com>
2023-09-25 12:58:27 +0100

    changing regional priority reference to overseas priority

--- api/metadata/constants.py
+++ api/metadata/constants.py
@@ -531,7 +531,7 @@ FEEDBACK_FORM_EXPERIENCED_ISSUES_ANSWERS = Choices(
 
 PRIORITY_LEVELS = Choices(
     ("NONE", ""),
-    ("REGIONAL", "Regional Priority"),
+    ("OVERSEAS", "Overseas Priority"),
     ("COUNTRY", "Country Priority"),
     ("WATCHLIST", "Watch list"),
 )
