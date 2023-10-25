cd004e756bed2ce6dc74017668ef3e32a667c818
christopherpettinga <chrispettinga@gmail.com>
2023-10-25 14:35:10 +0100

    Moving circleCI test parallelisation into circleCI directly, so as not to mess with normal test running.
    Removed explicity sentry call, all user group changes are logged and admin changes are logged as critical so sent to sentry

--- tests/user/test_serializers.py
+++ tests/user/test_serializers.py
@@ -14,8 +14,10 @@ class TestUserDetailSerializer(TestCase, UserFactoryMixin):
     def setUp(self) -> None:
         self.admin_group = Group.objects.get(name="Administrator")
         self.publisher_group = Group.objects.get(name="Publisher")
+        self.analyst_group = Group.objects.get(name="Analyst")
+        self.editor_group = Group.objects.get(name="Editor")
 
-    @patch("logging.Logger")
+    @patch("logging.Logger.critical")
     def test_administrator_granted_critical_log(self, patched_logger):
         normal_user = self.create_standard_user()
         serializer = UserDetailSerializer(
@@ -30,7 +32,7 @@ class TestUserDetailSerializer(TestCase, UserFactoryMixin):
             f"User {normal_user.id} has been granted Administrator access"
         )
 
-    @patch("logging.Logger")
+    @patch("logging.Logger.critical")
     def test_administrator_revoked_send_to_sentry(self, patched_logger):
         admin_user = self.create_admin()
         serializer = UserDetailSerializer(
@@ -43,4 +45,24 @@ class TestUserDetailSerializer(TestCase, UserFactoryMixin):
 
         assert patched_logger.called_once_with(
             f"User {admin_user.id} has been removed from Administrator group"
+        )
+
+    @patch("logging.Logger.info")
+    def test_group_modifications_logged(self, patched_logger):
+        admin_user = self.create_editor()
+
+        serializer = UserDetailSerializer(
+            instance=admin_user,
+            partial=True,
+            data={"groups": [{"id": self.publisher_group.id}]},
+        )
+        assert serializer.is_valid()
+        serializer.save()
+
+        assert patched_logger.call_count == 2
+        assert patched_logger.called_with(
+            "User 5 has been added to the following groups: {'Publisher'}"
+        )
+        assert patched_logger.called_with(
+            "User 5 has been removed from the following groups: {'Editor'}"
         )
