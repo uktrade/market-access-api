c486316efb63c447b172e4682d5ceab2cf0222b4
Christopher Pettinga <chrispettinga@gmail.com>
2023-10-30 14:53:35 +0000

    reverting metadata changes (#628)

--- api/healthcheck/views.py
+++ api/healthcheck/views.py
@@ -1,5 +1,6 @@
 import time
 
+from django.conf import settings
 from hawkrest import HawkAuthentication
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
@@ -10,8 +11,12 @@ from .checks import db_check
 
 
 class HealthCheckView(GenericAPIView):
-    authentication_classes = (HawkAuthentication,)
-    permission_classes = (IsAuthenticated,)
+    if settings.HAWK_ENABLED:
+        authentication_classes = (HawkAuthentication,)
+        permission_classes = (IsAuthenticated,)
+    else:
+        authentication_classes = ()
+        permission_classes = ()
 
     def get(self, request):
         data = {"duration": time.time() - request.start_time, "status": db_check()}
