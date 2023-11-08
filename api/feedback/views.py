c677261bda6b075d13ee0e5ab5ce1f58a79c26da
Feroze Rub <feroze@taridigital.com>
2023-11-08 14:50:21 +0000

    Feature/tss 1093/review access control (#640)
    * Add only creator can update to Feedback serialiser
    
    * Test for only author can update
    
    * Remove redundant test api client

--- api/feedback/views.py
+++ api/feedback/views.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 
+from api.core.permissions import IsCreatorOrReadOnly
 from api.feedback.serializers import FeedbackSerializer
 
 from .models import Feedback
@@ -8,3 +9,7 @@ from .models import Feedback
 class AddFeedbackViewSet(ModelViewSet):
     serializer_class = FeedbackSerializer
     queryset = Feedback.objects.all()
+    permission_classes = [IsCreatorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
