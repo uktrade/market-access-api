4148cab4ecbafd26a2ca7361c307690ac0dad125
Uka Osim <uka.osim@digital.trade.gov.uk>
2023-11-20 08:19:35 +0000

    changed priority level to a method field

--- api/metadata/constants.py
+++ api/metadata/constants.py
@@ -535,7 +535,6 @@ PRIORITY_LEVELS = Choices(
     ("REGIONAL", "Regional Priority"),
     ("COUNTRY", "Country Priority"),
     ("WATCHLIST", "Watch list"),
-    ("PB100", "Top 100 Priority"),
 )
 
 NEXT_STEPS_ITEMS_STATUS_CHOICES = Choices(
