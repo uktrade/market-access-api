b5fe41a27948712b15659c45a319b04fc8cad3b5
Asmund Bekker <asmundbekker@DBT000435.local>
2023-11-20 11:29:19 +0000

    Return assigne name, not dict

--- api/history/items/action_plans.py
+++ api/history/items/action_plans.py
@@ -137,9 +137,7 @@ class ActionPlanTaskAssignedToHistoryItem(ActionPlanTaskHistoryItem):
             record.user = backup_user
             record.save()
 
-        return {
-            "assigned_to": self._format_user(record.assigned_to),
-        }
+        return self._format_user(record.assigned_to).get('name')
 
 
 class ActionPlanTaskStakeholdersHistoryItem(ActionPlanTaskHistoryItem):
