eed99f4878dccf04d312059a4aa5b56c3b154e73
Asmund Bekker <asmundbekker@DBT000435.local>
2023-11-22 09:37:40 +0000

    TSS-840: Deeactive users management command

---
+++ api/user/management/commands/deactivate_users.py
@@ -0,0 +1,30 @@
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+
+import logging
+
+
+class Command(BaseCommand):
+    """
+    Management command to deactivate users.
+    """
+
+    help = "this command will remove specified users"
+
+    def add_arguments(self, parser):
+        parser.add_argument('--emails', type=str, help='CSV list of user emails', required=True)
+
+    def handle(self, *args, **options):
+        logging.info('Deactivating users')
+        UserModel = get_user_model()
+
+        user_email_list = options['emails'].split(',')
+
+        qs = UserModel.objects.filter(email__in=user_email_list, is_active=True)
+
+        qs.update(is_active=False)
+        newline = "\n"
+        logging.info(f'Deactivated Users:{newline}{newline.join([email for email in user_email_list])}')
+
+
+
---
+++ tests/user/management/commands/test_deactivate_users.py
@@ -0,0 +1,43 @@
+from django.contrib.auth import get_user_model
+from django.core.management import call_command
+from django.test import TestCase
+
+from api.core.test_utils import create_test_user
+
+
+class TestFixAllUsers(TestCase):
+    def setUp(self):
+        super().setUp()
+
+    def tearDown(self):
+        # self.helper_sso_patch.stop()
+        super().tearDown()
+
+    def test_deactivate_users(self):
+        user1 = create_test_user()
+        user2 = create_test_user()
+        user3 = create_test_user()
+
+        assert user1.is_active
+        assert user2.is_active
+        assert user3.is_active
+
+        UserModel = get_user_model()
+
+        assert UserModel.objects.all().count() == 3
+        assert UserModel.objects.filter(is_active=True).count() == 3
+
+        emails = [user1.email, user2.email]
+
+        call_command("deactivate_users", emails=','.join(emails))
+
+        assert UserModel.objects.all().count() == 3
+        assert UserModel.objects.filter(is_active=True).count() == 1
+
+        user1.refresh_from_db()
+        user2.refresh_from_db()
+        user3.refresh_from_db()
+
+        assert not user1.is_active
+        assert not user2.is_active
+        assert user3.is_active
