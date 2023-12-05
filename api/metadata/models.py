8d764b92cd2d574effbd6f0aa579f8c8a9ca3683
Asmund Bekker <asmundbekker@DBT000435.local>
2023-12-05 13:54:30 +0000

    format

--- api/metadata/models.py
+++ api/metadata/models.py
@@ -3,7 +3,6 @@ import json
 from django.db import models
 from django.db.models import Q
 from ordered_model.models import OrderedModel
-from simple_history.models import HistoricalRecords
 
 from api.core.models import BaseModel
 from api.metadata.constants import BARRIER_TYPE_CATEGORIES, OrganisationType
--- tests/barriers/test_history.py
+++ tests/barriers/test_history.py
@@ -1,8 +1,9 @@
 import logging
+from unittest.mock import patch
 
-from api.history.factqories.action_plans import ActionPlanTaskHistoryFactory
 from django.test import TestCase
 from freezegun import freeze_time
+from notifications_python_client import NotificationsAPIClient
 
 from api.action_plans.models import ActionPlan
 from api.assessment.models import EconomicAssessment
@@ -20,6 +21,7 @@ from api.history.factories import (
     PublicBarrierNoteHistoryFactory,
     TeamMemberHistoryFactory,
 )
+from api.history.factories.action_plans import ActionPlanTaskHistoryFactory
 from api.history.items.action_plans import get_default_user
 from api.history.models import CachedHistoryItem
 from api.interactions.models import Interaction, PublicBarrierNote
