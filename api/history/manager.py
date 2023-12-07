72fc0cd19987d2ad460ffa73102efe3ce8bf0af6
Asmund Bekker <asmundbekker@DBT000435.local>
2023-12-07 09:47:17 +0000

    Remove barrier history items

--- api/history/manager.py
+++ api/history/manager.py
@@ -6,7 +6,6 @@ from api.barriers.models import (
     ProgrammeFundProgressUpdate,
 )
 from api.history.factories import (
-    BarrierHistoryFactory,
     DeliveryConfidenceHistoryFactory,
     EconomicAssessmentHistoryFactory,
     EconomicImpactAssessmentHistoryFactory,
@@ -23,7 +22,9 @@ from api.history.factories.action_plans import (
     ActionPlanMilestoneHistoryFactory,
     ActionPlanTaskHistoryFactory,
 )
+from api.history.v2.enrichment import enrich_status
 from api.history.v2.service import (
+    FieldMapping,
     convert_v2_history_to_legacy_object,
     enrich_full_history,
 )
@@ -37,45 +38,57 @@ class HistoryManager:
     """
 
     @classmethod
-    def get_activity(cls, barrier, use_cache=False):
-        history = cls.get_barrier_history(
+    def get_activity(cls, barrier):
+        v2_barrier_history = Barrier.get_history(
             barrier_id=barrier.pk,
-            fields=["archived", "priority", "status"],
-            use_cache=use_cache,
+            fields=[
+                [
+                    "archived",
+                    "archived_reason",
+                    "archived_explanation",
+                    "unarchived_reason",
+                ],
+                [FieldMapping("priority__code", "priority"), "priority_summary"],
+                [
+                    "status",
+                    "status_date",
+                    "status_summary",
+                    "sub_status",
+                    "sub_status_other",
+                ],
+            ],
         )
+        enrich_status(v2_barrier_history)
+        history = convert_v2_history_to_legacy_object(v2_barrier_history)
+
         history += cls.get_economic_assessment_history(
             barrier_id=barrier.pk,
             fields=("rating",),
-            use_cache=use_cache,
         )
         history += cls.get_economic_impact_assessment_history(
             barrier_id=barrier.pk,
             fields=("impact",),
-            use_cache=use_cache,
         )
         history += cls.get_resolvability_assessment_history(
             barrier_id=barrier.pk,
             fields=("time_to_resolve",),
-            use_cache=use_cache,
         )
         history += cls.get_strategic_assessment_history(
             barrier_id=barrier.pk,
             fields=("scale",),
-            use_cache=use_cache,
         )
         return history
 
     @classmethod
-    def get_public_activity(cls, public_barrier, use_cache=False):
+    def get_public_activity(cls, public_barrier):
         history_items = HistoryManager.get_public_barrier_history(
             barrier_id=public_barrier.barrier_id,
             start_date=public_barrier.created_on + datetime.timedelta(seconds=1),
-            use_cache=use_cache,
         )
         return history_items
 
     @classmethod
-    def get_full_history(cls, barrier, ignore_creation_items=False, use_cache=False):
+    def get_full_history(cls, barrier, ignore_creation_items=False):
         if ignore_creation_items:
             start_date = barrier.reported_on
         else:
@@ -139,9 +152,7 @@ class HistoryManager:
         return history
 
     @classmethod
-    def get_economic_assessment_history(
-        cls, barrier_id, fields=(), start_date=None, use_cache=False
-    ):
+    def get_economic_assessment_history(cls, barrier_id, fields=(), start_date=None):
         return EconomicAssessmentHistoryFactory.get_history_items(
             barrier_id=barrier_id,
             fields=fields,
@@ -150,7 +161,7 @@ class HistoryManager:
 
     @classmethod
     def get_economic_impact_assessment_history(
-        cls, barrier_id, fields=(), start_date=None, use_cache=False
+        cls, barrier_id, fields=(), start_date=None
     ):
         return EconomicImpactAssessmentHistoryFactory.get_history_items(
             barrier_id=barrier_id,
@@ -159,19 +170,7 @@ class HistoryManager:
         )
 
     @classmethod
-    def get_barrier_history(
-        cls, barrier_id, fields=(), start_date=None, use_cache=False
-    ):
-        return BarrierHistoryFactory.get_history_items(
-            barrier_id=barrier_id,
-            fields=fields,
-            start_date=start_date,
-        )
-
-    @classmethod
-    def get_action_plans_history(
-        cls, barrier_id, fields=(), start_date=None, use_cache=False
-    ):
+    def get_action_plans_history(cls, barrier_id, fields=(), start_date=None):
 
         return [
             *ActionPlanHistoryFactory.get_history_items(
@@ -192,7 +191,7 @@ class HistoryManager:
         ]
 
     @classmethod
-    def get_notes_history(cls, barrier_id, fields=(), start_date=None, use_cache=False):
+    def get_notes_history(cls, barrier_id, fields=(), start_date=None):
         return NoteHistoryFactory.get_history_items(
             barrier_id=barrier_id,
             fields=fields,
@@ -200,9 +199,7 @@ class HistoryManager:
         )
 
     @classmethod
-    def get_delivery_confidence_history(
-        cls, barrier_id, fields=(), start_date=None, use_cache=False
-    ):
+    def get_delivery_confidence_history(cls, barrier_id, fields=(), start_date=None):
 
         return DeliveryConfidenceHistoryFactory.get_history_items(
             barrier_id=barrier_id,
@@ -211,9 +208,7 @@ class HistoryManager:
         )
 
     @classmethod
-    def get_public_barrier_history(
-        cls, barrier_id, fields=(), start_date=None, use_cache=False
-    ):
+    def get_public_barrier_history(cls, barrier_id, fields=(), start_date=None):
         return PublicBarrierHistoryFactory.get_history_items(
             barrier_id=barrier_id,
             fields=fields,
@@ -221,9 +216,7 @@ class HistoryManager:
         )
 
     @classmethod
-    def get_public_barrier_notes_history(
-        cls, barrier_id, fields=(), start_date=None, use_cache=False
-    ):
+    def get_public_barrier_notes_history(cls, barrier_id, fields=(), start_date=None):
         return PublicBarrierNoteHistoryFactory.get_history_items(
             barrier_id=barrier_id,
             fields=fields,
@@ -232,7 +225,7 @@ class HistoryManager:
 
     @classmethod
     def get_resolvability_assessment_history(
-        cls, barrier_id, fields=(), start_date=None, use_cache=False
+        cls, barrier_id, fields=(), start_date=None
     ):
         return ResolvabilityAssessmentHistoryFactory.get_history_items(
             barrier_id=barrier_id,
@@ -241,9 +234,7 @@ class HistoryManager:
         )
 
     @classmethod
-    def get_strategic_assessment_history(
-        cls, barrier_id, fields=(), start_date=None, use_cache=False
-    ):
+    def get_strategic_assessment_history(cls, barrier_id, fields=(), start_date=None):
         return StrategicAssessmentHistoryFactory.get_history_items(
             barrier_id=barrier_id,
             fields=fields,
@@ -251,7 +242,7 @@ class HistoryManager:
         )
 
     @classmethod
-    def get_team_history(cls, barrier_id, fields=(), start_date=None, use_cache=False):
+    def get_team_history(cls, barrier_id, fields=(), start_date=None):
         return TeamMemberHistoryFactory.get_history_items(
             barrier_id=barrier_id,
             fields=fields,
@@ -259,7 +250,7 @@ class HistoryManager:
         )
 
     @classmethod
-    def get_wto_history(cls, barrier_id, fields=(), start_date=None, use_cache=False):
+    def get_wto_history(cls, barrier_id, fields=(), start_date=None):
         return WTOHistoryFactory.get_history_items(
             barrier_id=barrier_id,
             fields=fields,
