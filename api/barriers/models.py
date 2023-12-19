b20460917eb10ac07022b25f9352780b590ff602
Uka Osim <uka.osim@digital.trade.gov.uk>
2023-12-19 17:58:24 +0000

    removing non mapped fields

--- api/barriers/models.py
+++ api/barriers/models.py
@@ -15,9 +15,7 @@ from django.contrib.postgres.search import SearchVector
 from django.core.cache import cache
 from django.core.validators import int_list_validator
 from django.db import models
-from django.db.models import CASCADE, CharField, F, Q, QuerySet
-from django.db.models import Value as V
-from django.db.models.functions import Concat
+from django.db.models import CASCADE, Q, QuerySet
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from django_filters.widgets import BooleanWidget
@@ -1360,8 +1358,6 @@ class PublicBarrier(FullyArchivableMixin, BaseModel):
                 "status",
                 "status_date",
             ],
-            FieldMapping("barrier__status", "status"),
-            FieldMapping("barrier__title", "title"),
         )
 
         # Get all fields required - raw changes no enrichment
@@ -1799,6 +1795,10 @@ class BarrierFilterSet(django_filters.FilterSet):
             value.remove("changed")
             public_queryset = queryset.filter(public_barrier__changed_since_public=True)
 
+        if "not_yet_sifted" in value:
+            value.remove("not_yet_sifted")
+            public_queryset = queryset.filter(public_eligibility=None)
+
         status_lookup = {
             "unknown": PublicBarrierStatus.UNKNOWN,
             "ineligible": PublicBarrierStatus.INELIGIBLE,
@@ -2042,31 +2042,7 @@ class PublicBarrierFilterSet(django_filters.FilterSet):
 
         if "changed" in value:
             value.remove("changed")
-            changed_ids = (
-                queryset.annotate(
-                    change=Concat(
-                        "barrier__cached_history_items__model",
-                        V("."),
-                        "barrier__cached_history_items__field",
-                        output_field=CharField(),
-                    ),
-                    change_date=F("barrier__cached_history_items__date"),
-                )
-                .filter(
-                    _public_view_status=PublicBarrierStatus.PUBLISHED,
-                    change_date__gt=F("last_published_on"),
-                    change__in=(
-                        "barrier.categories",
-                        "barrier.location",
-                        "barrier.sectors",
-                        "barrier.status",
-                        "barrier.summary",
-                        "barrier.title",
-                    ),
-                )
-                .values_list("id", flat=True)
-            )
-            public_queryset = queryset.filter(id__in=changed_ids)
+            public_queryset = queryset.filter(public_barrier__changed_since_public=True)
 
         if "not_yet_sifted" in value:
             value.remove("not_yet_sifted")
