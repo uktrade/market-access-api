a2b18472198dd3f86f96c6f125c7926991eebd10
Asmund Bekker <asmundbekker@DBT000435.local>
2024-01-30 14:42:36 +0000

    format

--- tests/barrier_reports/test_views.py
+++ tests/barrier_reports/test_views.py
@@ -2,8 +2,6 @@ import json
 from unittest import TestCase
 
 import mock
-from django.http import JsonResponse
-from freezegun import freeze_time
 from rest_framework import status
 from rest_framework.reverse import reverse
 
@@ -51,21 +49,23 @@ class TestBarrierReportViews(APITestMixin, TestCase):
 
         assert response.status_code == status.HTTP_201_CREATED
 
-    # @mock.patch('api.barrier_reports.service')
-    # def test_barrier_report_post_endpoint_success_and_retrieve(self, mock_service):
-    #     barrier = BarrierFactory()
-    #     barrier_report = BarrierReport.objects.create()
-    #     mock_service.create_barrier_report.return_value = barrier_report
-    #     url = reverse("barrier-reports")
-    #
-    #     response = self.api_client.post(url)
-    #
-    #     assert response.status_code == status.HTTP_201_CREATED
-    #     assert str(barrier_report.id) == json.loads(response.content)['barrier_report_id']
-    #
-    #     url = reverse("get-barrier-report", kwargs={'pk': str(barrier_report.id)})
-    #
-    #     response = self.api_client.get(url)
-    #
-    #     assert response.status_code == status.HTTP_200_OK
-    #     assert response.content == ''
+    @mock.patch("api.barrier_reports.service")
+    def test_barrier_report_post_endpoint_success_and_retrieve(self, mock_service):
+        barrier = BarrierFactory()
+        barrier_report = BarrierReport.objects.create()
+        mock_service.create_barrier_report.return_value = barrier_report
+        url = reverse("barrier-reports")
+
+        response = self.api_client.post(url)
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert (
+            str(barrier_report.id) == json.loads(response.content)["barrier_report_id"]
+        )
+
+        url = reverse("get-barrier-report", kwargs={"pk": str(barrier_report.id)})
+
+        response = self.api_client.get(url)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.content == ""
