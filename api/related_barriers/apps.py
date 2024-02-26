02ce9925d7e34516c4888c8e03f1e88bb6b6e128
abarolo <asmundbekker@DBT000435.local>
2024-02-26 09:31:08 +0000

    f

--- api/related_barriers/apps.py
+++ api/related_barriers/apps.py
@@ -8,6 +8,8 @@ class RelatedBarriersConfig(AppConfig):
     name = "api.related_barriers"
 
     def ready(self):
-        if settings.RELATED_BARRIER_DB_ON:
+        if settings.RELATED_BARRIER_DB_ON and model.db is None:
+            # Note: This can get called multiple times during lifetime of Django application. (startup and when
+            # management commands run). We need to prevent that if the model is already initiated.
             db = model.create_db()
             model.set_db(database=db)
