20f4314c58ee3abe1e57b0befbfdb7da71d29b53
abarolo <asmundbekker@DBT000435.local>
2024-03-22 08:55:03 +0000

    format

--- tests/barriers/signals/test_handlers.py
+++ tests/barriers/signals/test_handlers.py
@@ -9,9 +9,8 @@ pytestmark = [pytest.mark.django_db]
 def test_related_barrier_handler():
     barrier = BarrierFactory()
 
-    with mock.patch('api.barriers.signals.handlers.related_barrier_update_embeddings') as mock_handler:
-        with mock.patch('api.barriers.signals.handlers.manager') as mock_manager:
-            barrier.title = 'New Title'
-            barrier.save()
+    with mock.patch("api.barriers.signals.handlers.manager") as mock_manager:
+        barrier.title = "New Title"
+        barrier.save()
 
     assert mock_manager.manager.update_barrier.call_count == 1
