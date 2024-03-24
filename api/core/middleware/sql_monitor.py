7878b5e24e274acba796c5e0bb59a42086744072
abarolo <asmundbekker@DBT000435.local>
2024-03-24 22:07:33 +0000

    logger

--- api/core/middleware/sql_monitor.py
+++ api/core/middleware/sql_monitor.py
@@ -1,6 +1,9 @@
+import logging
 import time
 
 from django.db import connection
+
+logger = logging.getLogger(__name__)
 
 
 class SqlMonitor:
@@ -27,8 +30,8 @@ class SqlMonitorMiddleware:
         with connection.execute_wrapper(sql_monitor):
             response = self.get_response(request)
 
-        print(f"sql.endpoint: {request.path}")
-        print(f"sql.query_count: {sql_monitor.query_count}")
-        print(f"sql.query_time: {sql_monitor.query_time}")
+        logger.info(f"sql.endpoint: {request.path}")
+        logger.info(f"sql.query_count: {sql_monitor.query_count}")
+        logger.info(f"sql.query_time: {sql_monitor.query_time}")
 
         return response
