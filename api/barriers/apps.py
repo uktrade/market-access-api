34bfe6c5506cb0d14c158ea766381b3eb222a982
Santino Molinaro <85284133+Santino-Trade@users.noreply.github.com>
2024-07-01 10:46:03 +0100

    Removing code no longer used after implementing MAU (#816)
    * Removing code no longer used after implementing MAU
    
    * Updating migration numbers
    
    * Update to migration for existing barriers
    
    * Migration fix
    
    ---------
    
    Co-authored-by: santinomolinaro <santino.molinaro@mail>
    Co-authored-by: Feroze Rub <feroze@taridigital.com>

--- api/barriers/apps.py
+++ api/barriers/apps.py
@@ -7,47 +7,24 @@ class BarriersConfig(AppConfig):
     def ready(self):
         from django.db.models.signals import m2m_changed, post_save, pre_save
 
-        from .models import Barrier, PublicBarrier, PublicBarrierLightTouchReviews
+        from .models import Barrier
         from .signals.handlers import (
             barrier_categories_changed,
-            barrier_changed_after_published,
-            barrier_completion_percentage_changed,
             barrier_completion_top_priority_barrier_resolved,
-            barrier_organisations_changed,
             barrier_priority_approval_email_notification,
             barrier_tags_changed,
-            public_barrier_categories_changed,
-            public_barrier_content_update,
-            public_barrier_light_touch_reviews_changed,
             related_barrier_update_embeddings,
         )
 
         m2m_changed.connect(
-            barrier_organisations_changed, sender=Barrier.organisations.through
-        )
-        m2m_changed.connect(
             barrier_categories_changed, sender=Barrier.categories.through
         )
         m2m_changed.connect(barrier_tags_changed, sender=Barrier.tags.through)
-        m2m_changed.connect(
-            public_barrier_categories_changed, sender=PublicBarrier.categories.through
-        )
-
-        pre_save.connect(public_barrier_content_update, sender=PublicBarrier)
-
-        pre_save.connect(barrier_changed_after_published, sender=Barrier)
 
         pre_save.connect(related_barrier_update_embeddings, sender=Barrier)
-
-        post_save.connect(barrier_completion_percentage_changed, sender=Barrier)
 
         post_save.connect(
             barrier_completion_top_priority_barrier_resolved, sender=Barrier
         )
 
         post_save.connect(barrier_priority_approval_email_notification, sender=Barrier)
-
-        post_save.connect(
-            public_barrier_light_touch_reviews_changed,
-            sender=PublicBarrierLightTouchReviews,
-        )
