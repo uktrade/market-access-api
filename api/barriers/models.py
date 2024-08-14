0aeb398efd854dd5f31391219720c0af51aef919
EPedley <lizzypedley@gmail.com>
2024-08-14 15:23:08 +0100

    Enabling filtering for the FE

--- api/barriers/models.py
+++ api/barriers/models.py
@@ -1269,6 +1269,7 @@ class BarrierFilterSet(django_filters.FilterSet):
     status_date_resolved_in_full = django_filters.Filter(method="resolved_date_filter")
     delivery_confidence = django_filters.BaseInFilter(method="progress_status_filter")
     category = django_filters.BaseInFilter("categories", distinct=True)
+    policy_team = django_filters.BaseInFilter("policy_teams", distinct=True)
     top_priority = django_filters.BaseInFilter(method="tags_filter")
     priority = django_filters.BaseInFilter(method="priority_filter")
     top_priority_status = django_filters.BaseInFilter(
