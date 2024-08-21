e7f29727f1009768f1da197e2fe1ea507854bc46
abarolo <145478290+abarolo@users.noreply.github.com>
2024-08-21 18:09:47 +0100

    Security update + Policy Teams management (#871) (#872)
    * tss-2089: nltk dep
    * invalidate barrier cache on policy team management command
    
    
    ---------
    
    Co-authored-by: abarolo <asmundbekker@DBT000435.local>

--- api/barriers/management/commands/populate_barrier_policy_team.py
+++ api/barriers/management/commands/populate_barrier_policy_team.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 
 from django.core.management import BaseCommand
 
+from api.barriers import barrier_cache
 from api.barriers.models import Barrier
 
 
@@ -48,5 +49,6 @@ class Command(BaseCommand):
                 if dryrun:
                     print(f"{barrier_id}: {policy_team_ids}")
                 else:
+                    barrier_cache.delete(barrier_id)
                     for pid in policy_team_ids:
                         barrier.policy_teams.add(pid)
