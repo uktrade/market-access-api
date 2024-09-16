e65f68f033cf2ff426db97e33e06ceef5f46227e
Santino Molinaro <85284133+Santino-Trade@users.noreply.github.com>
2024-09-16 13:41:30 +0100

    Dashboard Parent Ticket (#896)
    * Adding free-text-search to related barriers (#845)
    
    * Adding free-text-search to related barriers
    
    * Rebase
    
    * Update and working search endpoint
    
    * format
    
    * format
    
    * Excluding archived barriers from inactive notification alerts (#858)
    
    Co-authored-by: santinomolinaro <santino.molinaro@mail>
    
    * Deleting cached barrier when creating and updating next steps items so they are reflected instantly on frontend (#861)
    
    Co-authored-by: santinomolinaro <santino.molinaro@mail>
    
    * TSS-1995: Move set_to_allowed_on into public barrier serializer (#862)
    
    * tss-1995: set_to_allowed expected on public_barrier serializer
    
    ---------
    
    Co-authored-by: abarolo <asmundbekker@DBT000435.local>
    
    * updates
    
    * format
    
    * format
    
    * using the full text search on the search page
    
    * lint fix
    
    ---------
    
    Co-authored-by: santinomolinaro <santino.molinaro@mail>
    Co-authored-by: abarolo <asmundbekker@DBT000435.local>
    Co-authored-by: abarolo <145478290+abarolo@users.noreply.github.com>
    Co-authored-by: Uka Osim <hsojo91@gmail.com>
    Co-authored-by: Uka Osim <uka.osim@digital.trade.gov.uk>
    
    * feat: lint fix
    
    * raise search limit
    
    * test fix
    
    * TSS 1966 dashboard summary with filters (#885)
    
    * hotfix: Reduce Dataworkspace pagination size (#877)
    
    * hotfix: Reduce Dataworkspace pagination size
    
    ---------
    
    Co-authored-by: abarolo <asmundbekker@DBT000435.local>
    
    * Basic summary info
    
    Basic summary stats exposed with filter set implemented
    
    * Fix filtering
    
    * Remove comments
    
    * Add financial year
    
    ---------
    
    Co-authored-by: abarolo <145478290+abarolo@users.noreply.github.com>
    Co-authored-by: abarolo <asmundbekker@DBT000435.local>
    
    * Tss 1965 dashboard tasks merge branch (#888)
    
    * Adding Dashboard Task list view to calculate outstanding tasks for display on new dashboard
    
    * Unit tests and edge case fixes
    
    * Limiting initial query to 1000 results, and order by last modified on date
    
    ---------
    
    Co-authored-by: santinomolinaro <santino.molinaro@mail>
    
    * Dashboard chart data (#898)
    
    * Fix PB100 filter (#890)
    
    ---------
    
    Co-authored-by: santinomolinaro <santino.molinaro@mail>
    Co-authored-by: abarolo <asmundbekker@DBT000435.local>
    Co-authored-by: abarolo <145478290+abarolo@users.noreply.github.com>
    Co-authored-by: Uka Osim <hsojo91@gmail.com>
    Co-authored-by: Uka Osim <uka.osim@digital.trade.gov.uk>
    Co-authored-by: Feroze Rub <feroze@taridigital.com>

---
+++ api/related_barriers/management/commands/run_search_related_test.py
@@ -0,0 +1,80 @@
+import logging
+
+from django.core.management import BaseCommand
+
+from api.barriers.models import Barrier
+from api.barriers.tasks import get_barriers_overseas_region
+from api.metadata.utils import get_sector
+from api.related_barriers.views import related_barriers_search
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+
+    # ./manage.py run_search_related_test --searchterm 'Company called BARCLAYS PLC is affected by this barrier'
+
+    help = "Test the related barriers engine with a search term"
+
+    def add_arguments(self, parser):
+        parser.add_argument("--searchterm", type=str, help="Search term to use")
+
+    def handle(self, *args, **options):
+        logger.critical("+++++++++")
+        logger.critical("Start Test")
+        logger.critical(options["searchterm"])
+        logger.critical("+++++++++")
+
+        logger.critical("SCORES -----")
+        response_data = related_barriers_search(
+            "request_dummy", options["searchterm"], log_score=True
+        )
+        logger.critical("------------")
+
+        for bar_id in response_data:  # [-6:]:
+            if bar_id != "search_term":
+                logger.critical("-")
+                barrier_detail = Barrier.objects.get(pk=bar_id)
+                logger.critical("ID = " + str(bar_id))
+                logger.critical("TITLE = " + str(barrier_detail.title))
+                logger.critical("SUMMARY = " + str(barrier_detail.summary))
+                sectors_list = [
+                    get_sector(sector)["name"] for sector in barrier_detail.sectors
+                ]
+                logger.critical(
+                    "SECTORS = "
+                    + str(get_sector(barrier_detail.main_sector)["name"])
+                    + " "
+                    + str(sectors_list)
+                )
+                logger.critical(
+                    "COUNTRY = "
+                    + str(
+                        get_barriers_overseas_region(
+                            barrier_detail.country, barrier_detail.trading_bloc
+                        )
+                    )
+                )
+                logger.critical("COMPANIES = " + str(barrier_detail.companies))
+                logger.critical(
+                    "OTHER COMPANIES = " + str(barrier_detail.related_organisations)
+                )
+                logger.critical(
+                    "STATUS SUMMARY = " + str(barrier_detail.status_summary)
+                )
+                logger.critical(
+                    "EST. RESOLUTION DATE = "
+                    + str(barrier_detail.estimated_resolution_date)
+                )
+                logger.critical(
+                    "EXPORT DESC. = " + str(barrier_detail.export_description)
+                )
+                notes_list = [
+                    note.text for note in barrier_detail.interactions_documents.all()
+                ]
+                logger.critical("NOTES = " + str(notes_list))
+                logger.critical("-")
+
+        logger.critical("+++++++++")
+        logger.critical("End Test")
+        logger.critical("+++++++++")
