08840500db66c32363af94266fd6f7387ccdea3c
santinomolinaro <santino.molinaro@mail>
2024-10-01 10:59:24 +0100

    Filter out archived barriers for dashboard task list

--- api/user/views.py
+++ api/user/views.py
@@ -174,7 +174,9 @@ class DashboardUserTasksView(generics.ListAPIView):
         # get the full list of barriers they should be getting task list for
         users_barriers = (
             Barrier.objects.filter(
-                Q(barrier_team__user=recipient) & Q(barrier_team__archived=False)
+                Q(barrier_team__user=recipient)
+                & Q(barrier_team__archived=False)
+                & Q(archived=False)
             )
             .distinct()
             .order_by("modified_on")[:1000]
