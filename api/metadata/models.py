72e1907b55468bacaac8ef61aa2e01bdc574a4a5
EPedley <lizzypedley@gmail.com>
2025-02-11 10:26:35 +0000

    Removing

--- api/metadata/models.py
+++ api/metadata/models.py
@@ -48,10 +48,6 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
-    # TODO: remove - looks like this is not being used
-    def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
-
 
 class BarrierTag(OrderedModel, BaseModel):
     """
